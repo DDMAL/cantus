02874b7d26faad4d7d410800a51b76b68c7a1fd3
Andrew Horwitz <ahwitz@gmail.com>
2014-05-12 15:00:02 -0400

    New: neume objects that point back to zones
    may reverse at some point

--- GXML2MEI/gameraXML2MEI.py
+++ GXML2MEI/gameraXML2MEI.py
@@ -2,11 +2,12 @@ import xmlDict
 import sys
 from pymei import MeiDocument, MeiElement, MeiAttribute, XmlExport
 import random
+from uuid import uuid4
 
 glyph_list = xmlDict.ConvertXmlToDict('page_glyphs.xml')['gamera-database']['glyphs']['glyph']
 
 def generate_MEI_ID():
-	return '%030x' % random.randrange(16**30)
+	return 'm-' + str(uuid4())
 
 #meiDict = {}
 #meiDict['mei'] = {'meiHead': {}, 'music': {}}
@@ -20,6 +21,7 @@ root.id = generate_MEI_ID()
 meiDocOut.root = root
 
 zones = []
+neumes = []
 
 for curGlyph in glyph_list:
 	startX = curGlyph['ulx']
@@ -37,7 +39,33 @@ for curGlyph in glyph_list:
 	zones[zoneIndex].addAttribute(MeiAttribute('lrx', endX))
 	zones[zoneIndex].addAttribute(MeiAttribute('lry', endY))
 
+	neumes.append(MeiElement('neume'))
+	neumeIndex = len(neumes) - 1
+
+	neumes[neumeIndex].id = generate_MEI_ID()
+	neumes[neumeIndex].addAttribute(MeiAttribute('facs', zones[zoneIndex].id))
+	neumes[neumeIndex].addAttribute(MeiAttribute('name', curGlyph['ids']['id']['name']))
+
+
+music = MeiElement('music')
+facsimile = MeiElement('facsimile')
+surface = MeiElement('surface')
+
+layout = MeiElement('layout') #gets added to music
+page = MeiElement('page') #gets added to layout
+#systems get added to page
+
+body = MeiElement('body')
+
+root.addChild(music)
+root.addChild(body)
+music.addChild(facsimile)
+facsimile.addChild(surface)
+
 for element in zones:
-	root.addChild(element)
+	surface.addChild(element)
+
+for element in neumes:
+	body.addChild(element)
 
 XmlExport.meiDocumentToFile(meiDocOut, 'testdoc.mei')
