0c1630d9c74f84032eee1ef95d35d8fdb9bdf110
Wei Gao <weigao2002@hotmail.com>
2014-05-13 11:19:10 -0400

    Added the entire project to iclude solr and documentation

--- public/admin/__init__.py
+++
@@ -1,1 +0,0 @@
-from cantusdata.admin.admin import *
--- public/serializers/chant.py
+++
@@ -1,7 +0,0 @@
-from cantusdata.models.chant import Chant
-from rest_framework import serializers
-
-
-class ChantSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Chant
