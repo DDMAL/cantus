24733fdde62cb9092edeb788973a4fc60b503a05
Andrew Horwitz <ahwitz@gmail.com>
2014-05-14 10:13:19 -0400

    New: "diva.html"
    -gx2m.py supports multiple files
    -included 6 pages of MEI files that line up with Diva CGI on ddmalwebserver
    -QOL updates for file structure

--- GXML2MEI/gameraXML2MEI.py
+++ GXML2MEI/gameraXML2MEI.py
@@ -1,71 +1,84 @@
 import xmlDict
-import sys
+import sys, os
 from pymei import MeiDocument, MeiElement, MeiAttribute, XmlExport
 import random
 from uuid import uuid4
+from optparse import OptionParser
 
-glyph_list = xmlDict.ConvertXmlToDict('page_glyphs.xml')['gamera-database']['glyphs']['glyph']
+#todo: have zone point to neume rather than vice versa
+
 
 def generate_MEI_ID():
 	return 'm-' + str(uuid4())
 
-#meiDict = {}
-#meiDict['mei'] = {'meiHead': {}, 'music': {}}
-#meiDict['mei']['meiHead'] = {'fileDesc': {'titleStmt': {'title': ''}, 'pubStmt': '',}}
-#meiDict['mei']['music'] = {'facsimile': {}, 'body':{}}
+'''usage = "%prog [options] input_directory output_directory data_output_directory"
+parser = OptionParser(usage)
+options, args = parser.parse_args()
 
-meiDocOut = MeiDocument()
+if len(args) < 1:
+    print("You must specify an input directory.")
+    sys.exit(-1)'''
 
-root = MeiElement("mei")
-root.id = generate_MEI_ID()
-meiDocOut.root = root
+fileList = [f for f in os.listdir('.') if (os.path.isfile(f) and f.endswith('.xml'))]
 
-zones = []
-neumes = []
+for xmlFile in fileList:
+	fileName = xmlFile[:-4]
+	glyph_list = xmlDict.ConvertXmlToDict(xmlFile)['gamera-database']['glyphs']['glyph']
+	#except indexerror for "not a gameraXML file"
+	meiDocOut = MeiDocument()
 
-for curGlyph in glyph_list:
-	startX = curGlyph['ulx']
-	endX = str(int(curGlyph['ulx']) + int(curGlyph['ncols']))
+	root = MeiElement("mei")
+	root.id = generate_MEI_ID()
+	meiDocOut.root = root
 
-	startY = curGlyph['uly']
-	endY = str(int(curGlyph['uly']) + int(curGlyph['nrows']))
+	zones = []
+	neumes = []
 
-	zones.append(MeiElement('zone'))
-	zoneIndex = len(zones) - 1
+	for curGlyph in glyph_list:
+		startX = curGlyph['ulx']
+		endX = str(int(curGlyph['ulx']) + int(curGlyph['ncols']))
 
-	zones[zoneIndex].id = generate_MEI_ID()
-	zones[zoneIndex].addAttribute(MeiAttribute('ulx', startX))
-	zones[zoneIndex].addAttribute(MeiAttribute('uly', startY))
-	zones[zoneIndex].addAttribute(MeiAttribute('lrx', endX))
-	zones[zoneIndex].addAttribute(MeiAttribute('lry', endY))
+		startY = curGlyph['uly']
+		endY = str(int(curGlyph['uly']) + int(curGlyph['nrows']))
 
-	neumes.append(MeiElement('neume'))
-	neumeIndex = len(neumes) - 1
+		neumes.append(MeiElement('neume'))
+		neumeIndex = len(neumes) - 1
 
-	neumes[neumeIndex].id = generate_MEI_ID()
-	neumes[neumeIndex].addAttribute(MeiAttribute('facs', zones[zoneIndex].id))
-	neumes[neumeIndex].addAttribute(MeiAttribute('name', curGlyph['ids']['id']['name']))
+		neumes[neumeIndex].id = generate_MEI_ID()
+		neumes[neumeIndex].addAttribute(MeiAttribute('name', curGlyph['ids']['id']['name']))
+
+		zones.append(MeiElement('zone'))
+		zoneIndex = len(zones) - 1
+
+		zones[zoneIndex].id = generate_MEI_ID()
+		zones[zoneIndex].addAttribute(MeiAttribute('neume', neumes[neumeIndex].id))
+		zones[zoneIndex].addAttribute(MeiAttribute('ulx', startX))
+		zones[zoneIndex].addAttribute(MeiAttribute('uly', startY))
+		zones[zoneIndex].addAttribute(MeiAttribute('lrx', endX))
+		zones[zoneIndex].addAttribute(MeiAttribute('lry', endY))
 
 
-music = MeiElement('music')
-facsimile = MeiElement('facsimile')
-surface = MeiElement('surface')
+	#needs meiHead here
+	music = MeiElement('music')
+	facsimile = MeiElement('facsimile')
+	surface = MeiElement('surface')
 
-layout = MeiElement('layout') #gets added to music
-page = MeiElement('page') #gets added to layout
-#systems get added to page
+	layout = MeiElement('layout') #gets added to music
+	page = MeiElement('page') #gets added to layout
+	#systems get added to page
+	#neumes get added to systems
 
-body = MeiElement('body')
+	body = MeiElement('body')
 
-root.addChild(music)
-music.addChild(facsimile)
-music.addChild(body)
-facsimile.addChild(surface)
+	root.addChild(music)
+	music.addChild(facsimile)
+	music.addChild(body)
+	facsimile.addChild(surface)
 
-for element in zones:
-	surface.addChild(element)
+	for element in zones:
+		surface.addChild(element)
 
-for element in neumes:
-	body.addChild(element)
+	for element in neumes:
+		body.addChild(element)
 
-XmlExport.meiDocumentToFile(meiDocOut, 'testdoc.mei')
+	XmlExport.meiDocumentToFile(meiDocOut, fileName+'.mei')
