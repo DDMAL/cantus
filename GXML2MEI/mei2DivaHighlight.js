7150e5a3ad469f2cd23df5771a738944c6125426
Andrew Horwitz <ahwitz@gmail.com>
2014-05-15 12:52:05 -0400

    New: Saving works.
    Very rudimentary but it will save files.

--- GXML2MEI/mei2DivaHighlight.js
+++ GXML2MEI/mei2DivaHighlight.js
@@ -6,32 +6,39 @@ Requires:
 Usage:
 	-Read a selection of MEI files using the JavaScript FileReader object's readAsText() method
 		(new FileReader()).readAsText(files[0])
+	-Pass these MEI files into createHighlights()
 */
 
 var neumeObjects = [];
 var currentTarget;
 
-function createHighlights(meiArray){
-	var x2js = new X2JS(); //from plugin
-	for(var pageIndex = 0; pageIndex < meiArray.length; pageIndex++)
-	{
-		jsonData = x2js.xml_str2json(meiArray[pageIndex]);
+/*
+    Creates highlight regions from MEI text.
+    @param meiArray Array of mei files in raw text
+*/
+function createHighlights(meiArray)
+{
+	var x2js = new X2JS(); //from xml2json.js
+	var pageIndex = meiArray.length;
+	while (pageIndex--)
+	{ //for each page
+		jsonData = x2js.xml_str2json(meiArray[pageIndex]); //convert to json
 		regions = [];
 
-		xmlns = jsonData['mei']['_xmlns']
+		xmlns = jsonData['mei']['_xmlns'] //find the xml namespace file
 		var neume_ulx, neume_uly, neume_width, neume_height;
 		neumeArray = jsonData['mei']['music']['body']['neume'];
 		facsArray = jsonData['mei']['music']['facsimile']['surface']['zone'];
 
-		for(curZoneIndex in facsArray)
-		{
+		for (curZoneIndex in facsArray)
+		{ //for each "zone" object
 			curZone = facsArray[curZoneIndex];
 			neumeID = curZone._neume;
-			for(curNeumeIndex in neumeArray)
-			{
+			for (curNeumeIndex in neumeArray)
+			{ //find the corresponding neume - don't think there's a more elegant way in JS
 				if (neumeArray[curNeumeIndex]["_xml:id"] == neumeID)
 				{
-					curNeume = neumeArray[curNeumeIndex];
+					curNeume = neumeArray[curNeumeIndex]; //assemble the info on the neume
 					neumeObjects[neumeID] = curNeume['_name']
 					neume_ulx = curZone._ulx;
 					neume_uly = curZone._uly;
@@ -40,20 +47,25 @@ function createHighlights(meiArray){
 					break;
 				}
 			}
+			//add it to regions
 			regions.push({'width': neume_width, 'height': neume_height, 'ulx': neume_ulx, 'uly': neume_uly, 'divID': neumeID});
 		}
+		//at the end of each page, call the highlights
 		dv.highlightOnPage(pageIndex, regions, undefined, "overlay-box", reapplyHoverListener);
 	}
 }
-
-function reapplyHoverListener(){
+/*
+	Function to be called when everytime the highlights are regenerated to regenerate the on-hover effect
+*/
+function reapplyHoverListener()
+{
 	$(".overlay-box").hover(function(e)
 	{
 		currentTarget = e.target.id;
 
 		$("#hover-div").html(neumeObjects[currentTarget]);
 		$("#hover-div").css(
-		{
+		{//create a div with the name of the hovered neume
 			'top': e.pageY - 50,
 			'height': 40,
 			'left': e.pageX,
@@ -64,10 +76,11 @@ function reapplyHoverListener(){
 			'display': 'block',
 			'vertical-align': 'middle',
 		});
-		$("#"+currentTarget).css('background-color', 'rgba(255, 255, 255, 0.2)');
+		//change the color of the hovered div
+		$("#"+currentTarget).css('background-color', 'rgba(255, 255, 255, 0.05)');
 
 		$(document).on('mousemove', function(e)
-		{
+		{//have it follow the mouse
 			$("#hover-div").css(
 			{
 				'top': e.pageY - 50,
@@ -79,7 +92,7 @@ function reapplyHoverListener(){
 		$(document).unbind('mousemove');
 		$("#hover-div").css('display', 'none');
 
-		$("#"+currentTarget).css('background-color', 'rgba(255, 0, 0, 0.5)');
+		$("#"+currentTarget).css('background-color', 'rgba(255, 0, 0, 0.2)');
 		$("#hover-div").html("");
 	});
 }
