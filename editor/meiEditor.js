ee530c28e90b19a57779ef3b8c394b996d946474
Andrew Horwitz <ahwitz@gmail.com>
2014-05-15 16:20:31 -0400

    New: moved files to "editor"
    Moved files to where they belong (as this is editor interface work), also combined meiEditor.js and mei2DivaHighlights.js

---
+++ editor/meiEditor.js
@@ -0,0 +1,138 @@
+var AceMeiEditor = function(){
+	var meiArray = [];
+    var activeDoc;
+    var currentPage;
+    var currentDocPosition = {'row': 1, 'col': 1};
+    var pageData = {};
+    var pageLocToData = {};
+    var neumeObjects = [];
+    var currentTarget;
+
+	_init = function()
+	{
+        activeDoc = editor.getSession().doc; //get a reference to the active Document
+    }
+
+    createHighlights = function()
+	{
+		function reapplyHoverListener()
+		{
+			$(".overlay-box").hover(function(e)
+			{
+				currentTarget = e.target.id;
+
+				$("#hover-div").html(neumeObjects[currentTarget]);
+				$("#hover-div").css(
+				{//create a div with the name of the hovered neume
+					'top': e.pageY - 50,
+					'height': 40,
+					'left': e.pageX,
+					'padding-left': '10px',
+					'padding-right': '10px',
+					'border': 'thin black solid',
+					'background': '#FFFFFF',
+					'display': 'block',
+					'vertical-align': 'middle',
+				});
+				//change the color of the hovered div
+				$("#"+currentTarget).css('background-color', 'rgba(255, 255, 255, 0.05)');
+
+				$(document).on('mousemove', function(e)
+				{//have it follow the mouse
+					$("#hover-div").css(
+					{
+						'top': e.pageY - 50,
+						'left': e.pageX,
+					});
+				});
+			}, function(e){
+				currentTarget = e.target.id;
+				$(document).unbind('mousemove');
+				$("#hover-div").css('display', 'none');
+
+				$("#"+currentTarget).css('background-color', 'rgba(255, 0, 0, 0.2)');
+				$("#hover-div").html("");
+			});
+		}
+		
+		var x2js = new X2JS(); //from xml2json.js
+		var pageIndex = meiArray.length;
+		while (pageIndex--)
+		{ //for each page
+			jsonData = x2js.xml_str2json(meiArray[pageIndex]); //convert to json
+			regions = [];
+
+			xmlns = jsonData['mei']['_xmlns'] //find the xml namespace file
+			var neume_ulx, neume_uly, neume_width, neume_height;
+			neumeArray = jsonData['mei']['music']['body']['neume'];
+			facsArray = jsonData['mei']['music']['facsimile']['surface']['zone'];
+
+			for (curZoneIndex in facsArray)
+			{ //for each "zone" object
+				curZone = facsArray[curZoneIndex];
+				neumeID = curZone._neume;
+				for (curNeumeIndex in neumeArray)
+				{ //find the corresponding neume - don't think there's a more elegant way in JS
+					if (neumeArray[curNeumeIndex]["_xml:id"] == neumeID)
+					{
+						curNeume = neumeArray[curNeumeIndex]; //assemble the info on the neume
+						neumeObjects[neumeID] = curNeume['_name']
+						neume_ulx = curZone._ulx;
+						neume_uly = curZone._uly;
+						neume_width = curZone._lrx - neume_ulx;
+						neume_height = curZone._lry - neume_uly;
+						break;
+					}
+				}
+				//add it to regions
+				regions.push({'width': neume_width, 'height': neume_height, 'ulx': neume_ulx, 'uly': neume_uly, 'divID': neumeID});
+			}
+			//at the end of each page, call the highlights
+			dv.highlightOnPage(pageIndex, regions, undefined, "overlay-box", reapplyHoverListener);
+		}
+	}
+
+    this.changeActivePage = function(pageName)
+    {
+    	editor.setSession(pageData[pageName]); //inserts text
+        activeDoc = editor.getSession().doc;
+    }
+
+    this.savePageToClient = function(pageName)
+    {
+    	formatToSave = function(lineIn, indexIn)
+    	{          
+        	if(lineIn !== "")
+    		{
+        		formattedData[indexIn] = lineIn + "\n";
+        	}
+    	}
+
+        var formattedData = [];
+        var lastRow = pageData[pageName].doc.getLength() - 1; //no row 0, rest are 0-index though?
+        pageData[pageName].doc.getLines(0, lastRow).forEach(formatToSave);
+        formattedData[formattedData.length - 1].trim()
+        var pageBlob = new Blob(formattedData, {type: "text/plain;charset=utf-8"});
+        saveAs(pageBlob, pageName);
+	  
+    }	
+
+    this.addPage = function(pageDataIn, fileNameIn)
+    {
+    	var tempIndex = meiArray.push(pageDataIn) - 1;
+        pageData[fileNameIn] = new ace.EditSession(pageDataIn, "ace/mode/xml"); //add the file's data into a "pageData" array that will eventually feed into the ACE editor
+        pageLocToData[fileNameIn] = tempIndex; //because I have to keep the meiArray sequential, populate it into an array that transfers page titles into meiArray sequence
+    }
+
+	this.reloadFromACE = function()
+    {
+        meiArray[pageLocToData[currentPage]] = activeDoc.getAllLines().join("\n"); //
+        createHighlights(meiArray);
+    }
+
+    this.getMeiArray = function(){
+    	return meiArray;
+    }
+
+    _init();
+}
