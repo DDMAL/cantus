8d56e0fb9ce44736e6ef7db50f7effccce268cdc
Andrew Horwitz <ahwitz@gmail.com>
2014-05-16 11:41:28 -0400

    New: halfway done with conversion to plugin

--- editor/meiEditor.js
+++ editor/meiEditor.js
@@ -1,17 +1,36 @@
-var AceMeiEditor = function(aceEditor){
-    var activeDoc = aceEditor.getSession().doc;
-    var currentPage;
-    var currentDocPosition = {'row': 1, 'col': 1};
-    var pageData = {};
-    var orderedPageData = [];
-    var neumeObjects = [];
-    var currentTarget;
+(function ($)
+{
+	var AceMeiEditor = function(element, aceEditor){
+		var element = $(element);
+		var self = this;
+	    var activeDoc = aceEditor.getSession().doc;
+	    var currentPage;
+	    var currentDocPosition = {'row': 1, 'col': 1};
+	    var pageData = {};
+	    var orderedPageData = [];
+	    var neumeObjects = [];
+	    var currentTarget;
+	    var dv;
+	    var editor;
 
-    this.createHighlights = function()
-	{
-		function reapplyHoverListener()
-		{
-			$(".overlay-box").hover(function(e)
+	    var reapplyLoadSaveListeners = function()
+	    {
+	    	$(".meiLoad").on('click', function(e)
+	    	{
+				fileName = e.target.id.substring(4); //clips off "load" word from ID, there's probably a more graceful way
+				self.changeActivePage(fileName);
+			});
+
+			$(".meiSave").on('click', function()
+			{
+				fileName = e.target.id.substring(4); //clips off "save" word from ID
+				self.savePageToClient(fileName);
+			});
+	    }
+
+	    var reapplyHoverListener = function()
+	    {
+	    	$(".overlay-box").hover(function(e)
 			{
 				currentTarget = e.target.id;
 
@@ -47,84 +66,194 @@ var AceMeiEditor = function(aceEditor){
 				$("#"+currentTarget).css('background-color', 'rgba(255, 0, 0, 0.2)');
 				$("#hover-div").html("");
 			});
-		}
-		
-		var x2js = new X2JS(); //from xml2json.js
-		var pageIndex = orderedPageData.length;
-		while(pageIndex--)
-		{ //for each page
-			curPage = orderedPageData[pageIndex];
-			jsonData = x2js.xml_str2json(pageData[curPage].doc.getAllLines().join("\n"));
-			regions = [];
+	    }
 
-			xmlns = jsonData['mei']['_xmlns'] //find the xml namespace file
-			var neume_ulx, neume_uly, neume_width, neume_height;
-			neumeArray = jsonData['mei']['music']['body']['neume'];
-			facsArray = jsonData['mei']['music']['facsimile']['surface']['zone'];
+	    this.createHighlights = function()
+		{			
+			var x2js = new X2JS(); //from xml2json.js
+			var pageIndex = orderedPageData.length;
+			while(pageIndex--)
+			{ //for each page
+				curPage = orderedPageData[pageIndex];
+				jsonData = x2js.xml_str2json(pageData[curPage].doc.getAllLines().join("\n"));
+				regions = [];
 
-			for (curZoneIndex in facsArray)
-			{ //for each "zone" object
-				curZone = facsArray[curZoneIndex];
-				neumeID = curZone._neume;
-				for (curNeumeIndex in neumeArray)
-				{ //find the corresponding neume - don't think there's a more elegant way in JS
-					if (neumeArray[curNeumeIndex]["_xml:id"] == neumeID)
-					{
-						curNeume = neumeArray[curNeumeIndex]; //assemble the info on the neume
-						neumeObjects[neumeID] = curNeume['_name']
-						neume_ulx = curZone._ulx;
-						neume_uly = curZone._uly;
-						neume_width = curZone._lrx - neume_ulx;
-						neume_height = curZone._lry - neume_uly;
-						break;
+				xmlns = jsonData['mei']['_xmlns'] //find the xml namespace file
+				var neume_ulx, neume_uly, neume_width, neume_height;
+				neumeArray = jsonData['mei']['music']['body']['neume'];
+				facsArray = jsonData['mei']['music']['facsimile']['surface']['zone'];
+
+				for (curZoneIndex in facsArray)
+				{ //for each "zone" object
+					curZone = facsArray[curZoneIndex];
+					neumeID = curZone._neume;
+					for (curNeumeIndex in neumeArray)
+					{ //find the corresponding neume - don't think there's a more elegant way in JS
+						if (neumeArray[curNeumeIndex]["_xml:id"] == neumeID)
+						{
+							curNeume = neumeArray[curNeumeIndex]; //assemble the info on the neume
+							neumeObjects[neumeID] = curNeume['_name']
+							neume_ulx = curZone._ulx;
+							neume_uly = curZone._uly;
+							neume_width = curZone._lrx - neume_ulx;
+							neume_height = curZone._lry - neume_uly;
+							break;
+						}
 					}
+					//add it to regions
+					regions.push({'width': neume_width, 'height': neume_height, 'ulx': neume_ulx, 'uly': neume_uly, 'divID': neumeID});
 				}
-				//add it to regions
-				regions.push({'width': neume_width, 'height': neume_height, 'ulx': neume_ulx, 'uly': neume_uly, 'divID': neumeID});
+				//at the end of each page, call the highlights
+				dv.highlightOnPage(pageIndex, regions, undefined, "overlay-box", reapplyHoverListener);
 			}
-			//at the end of each page, call the highlights
-			dv.highlightOnPage(pageIndex, regions, undefined, "overlay-box", reapplyHoverListener);
 		}
+
+	    this.changeActivePage = function(pageName)
+	    {
+	    	editor.setSession(pageData[pageName]); //inserts text
+	        activeDoc = editor.getSession().doc;
+	    }
+
+	    this.reorderFiles = function(newOrder)
+	    {
+	    	var curPage = 0;
+	    	while(curPage < newOrder.length){
+	    		orderedPageData.push(newOrder[curPage]);
+	    		curPage++;
+	    	}
+	    }
+
+	    this.savePageToClient = function(pageName)
+	    {
+	    	formatToSave = function(lineIn, indexIn)
+	    	{          
+	        	if(lineIn !== "")
+	    		{
+	        		formattedData[indexIn] = lineIn + "\n";
+	        	}
+	    	}
+
+	        var formattedData = [];
+	        var lastRow = pageData[pageName].doc.getLength() - 1; //no row 0, rest are 0-index though?
+	        pageData[pageName].doc.getLines(0, lastRow).forEach(formatToSave);
+	        formattedData[formattedData.length - 1].trim()
+	        var pageBlob = new Blob(formattedData, {type: "text/plain;charset=utf-8"});
+	        saveAs(pageBlob, pageName);
+		  
+	    }	
+
+	    this.addPage = function(pageDataIn, fileNameIn)
+	    {
+	        pageData[fileNameIn] = new ace.EditSession(pageDataIn, "ace/mode/xml"); //add the file's data into a "pageData" array that will eventually feed into the ACE editor
+	        orderedPageData.push(fileNameIn); //keep track of the page orders to push the right highlights to the right pages
+	    }
+
+	    var _init = function()
+	    {
+
+	    	element.append('<div id="editor"></div>'
+    			+'<div id="diva-wrapper"></div>'
+    			+'<div class="clear"></div>'
+    			+'<div id="mei-editor"></div>'
+    			+'<span id="hover-div"></span><div id="file-upload">'
+        		+'<div id="file-upload-contents-wrapper">'
+            	+'<input type="file" value="Add a new file" id="fileInput">'
+            	+'<div id="file-list">Files loaded:<br></div>'
+            	+'<button id="updateDiva">Update DIVA</button>'
+            	+'<button id="minimize" style="float:right;">Minimize</button>'
+        		+'</div>'
+        		+'<div id="file-upload-minimized-wrapper" style="display:none;">'
+            	+'<span id="file-list">Files loaded:</span>'
+            	+'<button id="maximize" style="float:right;">Maximize</button>'
+        		+'</div>'
+    			+'</div>');
+	    	$('#diva-wrapper').diva(
+	        {//create the diva wrapper
+	            contained: true,
+	            enableAutoHeight: true,
+	            fixedHeightGrid: false,
+	            iipServerURL: "http://132.206.14.136:8000/fcgi-bin/iipsrv.fcgi",
+	            objectData: "imagesOut.json",
+	            imageDir: "/opt/stgall",
+	            enableHighlight: true
+	        });
+	        dv = $('#diva-wrapper').data('diva');
+	        editor = ace.edit("editor"); //create the ACE editor
+	        editor.setTheme("ace/theme/ambiance");
+	        editor.getSession().setMode("ace/mode/xml");
+
+	    	$("#updateDiva").on('click', self.createHighlights);
+
+	    	$("#minimize").on('click', function(){
+	    		previousWidth = $("#file-upload").width();
+	            $("#file-upload-minimized-wrapper").css('display', 'block');
+	            $("#file-upload-contents-wrapper").css('display', 'none');
+	            $("#file-upload").width(previousWidth);
+	    	});
+
+	    	$("#maximize").on('click', function(){
+	    		$("#file-upload-contents-wrapper").css('display', 'block');
+	            $("#file-upload-minimized-wrapper").css('display', 'none');
+	        });
+
+	        $('#fileInput').change(function(e)
+	        { 
+	            var reader = new FileReader();
+	            reader.file = this.files[0];
+
+	            //when the file is loaded as text
+	            reader.onload = function(e) 
+	            { 
+	                fileName = this.file.name
+	                self.addPage(this.result, fileName);
+	                //$("#file-list").html($("#file-list").html()+"<span class='meiFile'><span class='meiName'>"+this.file.name+"</span><span class='meiLoad' onclick='loadPage(\""+this.file.name+"\")'>Load</span><span class='meiSave' onclick='savePage(\""+this.file.name+"\")'>Save</span></span>"); //add the file to the GUI    
+	                $("#file-list").html($("#file-list").html()
+	                	+"<div class='meiFile' id='"+fileName+"'>"+fileName
+	                	+"<button class='meiLoad' id='load"+fileName+"'>Load</button>"
+	                	+"<button class='meiSave' id='save"+fileName+"'>Save</button>"
+	                	+"</div>"); //add the file to the GUI
+	                reapplyLoadSaveListeners();
+	            };
+	            reader.readAsText(this.files[0]);
+	        });
+
+        	//various jQueryUI designators
+	        $("#file-upload").draggable();
+	        $("#file-list").sortable();
+	        $("#file-list").disableSelection();
+	        $("#file-list").on("sortstop", function(e, ui){
+	            fileList = $(".meiFile");
+	            newOrder = [];
+	            numberOfFiles = $(".meiFile").length;
+	            for(curFileIndex = 0; curFileIndex < numberOfFiles; curFileIndex++)
+	            {
+	                newOrder.push(fileList[curFileIndex].id);
+	            }
+	            meiEditor.reorderFiles(newOrder);
+	        });
+	    }    
+
+	    _init();
+
 	}
 
-    this.changeActivePage = function(pageName)
+    $.fn.AceMeiEditor = function (options)
     {
-    	editor.setSession(pageData[pageName]); //inserts text
-        activeDoc = editor.getSession().doc;
-    }
+        return this.each(function ()
+        {
+            var element = $(this);
 
-    this.reorderFiles = function(newOrder)
-    {
-    	var curPage = 0;
-    	while(curPage < newOrder.length){
-    		orderedPageData.push(newOrder[curPage]);
-    		curPage++;
-    	}
-    }
+            // Return early if this element already has a plugin instance
+            if (element.data('AceMeiEditor'))
+                return;
 
-    this.savePageToClient = function(pageName)
-    {
-    	formatToSave = function(lineIn, indexIn)
-    	{          
-        	if(lineIn !== "")
-    		{
-        		formattedData[indexIn] = lineIn + "\n";
-        	}
-    	}
+            // Save the reference to the container element
+            options.parentSelector = element;
 
-        var formattedData = [];
-        var lastRow = pageData[pageName].doc.getLength() - 1; //no row 0, rest are 0-index though?
-        pageData[pageName].doc.getLines(0, lastRow).forEach(formatToSave);
-        formattedData[formattedData.length - 1].trim()
-        var pageBlob = new Blob(formattedData, {type: "text/plain;charset=utf-8"});
-        saveAs(pageBlob, pageName);
-	  
-    }	
+            // Otherwise, instantiate the document viewer
+            var meiEditor = new AceMeiEditor(this, options);
+            element.data('AceMeiEditor', meiEditor);
+        });
+    };
 
-    this.addPage = function(pageDataIn, fileNameIn)
-    {
-        pageData[fileNameIn] = new ace.EditSession(pageDataIn, "ace/mode/xml"); //add the file's data into a "pageData" array that will eventually feed into the ACE editor
-        orderedPageData.push(fileNameIn); //keep track of the page orders to push the right highlights to the right pages
-    }
-
-}
+})(jQuery);
