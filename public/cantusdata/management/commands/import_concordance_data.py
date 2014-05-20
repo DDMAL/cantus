bad39179a858c6138fb0b778992c5c7ca48c60b1
AFFogarty <andrew.f.fogarty@gmail.com>
2014-05-20 10:40:59 -0400

    New: Error messages on import commands

--- public/cantusdata/management/commands/import_concordance_data.py
+++ public/cantusdata/management/commands/import_concordance_data.py
@@ -1,16 +1,25 @@
+from django.core.management.base import BaseCommand
 from cantusdata.models import Concordance
-from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
     args = ""
+    debug = True
 
     def handle(self, *args, **kwargs):
         """
         """
-        file = open("data_dumps/" + str(args[0]))
-        # Nuke the db concordances
-        Concordance.objects.all().delete()
+        if args:
+            file_name = str(args[0])
+        else:
+            return self.stdout.write("Please provide a file name!")
+        try:
+            file = open("data_dumps/" + file_name)
+        except IOError:
+            return self.stdout.write(u"File {0} does not exist!".format(file_name))
+        if self.debug:
+            # Nuke the db concordances
+            Concordance.objects.all().delete()
         # Every line is a new concordance
         for line in file.readlines():
             # This method is pretty hacky, but it seems to work
--- public/cantusdata/management/commands/import_manuscript_data.py
+++ public/cantusdata/management/commands/import_manuscript_data.py
@@ -1,23 +1,29 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
+from django.core.management.base import BaseCommand
 from cantusdata.models.manuscript import Manuscript
-import re
 import csv
 
 
 class Command(BaseCommand):
     args = ""
+    debug = True
 
     def handle(self, *args, **kwargs):
         """
         Run "python manage.py import_manuscript_data filename.csv" to import
         a manuscript file into the db.  filename.csv must exist in /public/data_dumps/.
         """
-        csv_file_name = args[0]
-        # Nuke the db manuscripts
-        Manuscript.objects.all().delete()
+        if args:
+            csv_file_name = args[0]
+        else:
+            return self.stdout.write("Please provide a file name!")
+        if self.debug:
+            # Nuke the db manuscripts
+            Manuscript.objects.all().delete()
         # Load in the csv file.  This is a massive list of dictionaries.
-        csv_file = csv.DictReader(open("data_dumps/" + str(csv_file_name), "rU"))
+        try:
+            csv_file = csv.DictReader(open("data_dumps/" + str(csv_file_name), "rU"))
+        except IOError:
+            return self.stdout.write(u"File {0} does not exist!".format(csv_file_name))
 
         # Create a manuscript and save it
         for row in csv_file:
