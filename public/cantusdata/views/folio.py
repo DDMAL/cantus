41b416cc04b12b06501c497c9cb5fbe111348eb9
AFFogarty <andrew.f.fogarty@gmail.com>
2014-05-22 13:42:09 -0400

    Fixed: Folio view no longer uses Concordance serializer

--- public/cantusdata/views/folio.py
+++ public/cantusdata/views/folio.py
@@ -1,27 +1,27 @@
 from cantusdata.models.folio import Folio
-from cantusdata.serializers.concordance import ConcordanceSerializer
+from cantusdata.serializers.folio import FolioSerializer
 from cantusdata.renderers.custom_html_renderer import CustomHTMLRenderer
 from rest_framework import generics
 from rest_framework.renderers import JSONRenderer, JSONPRenderer
 
 
-class FolioListHTMLRenderer(CustomHTMLRenderer):
-    template_name = "folio/folio_list.html"
-
-
-class FolioDetailHTMLRenderer(CustomHTMLRenderer):
-    template_name = "concordance/concordance_detail.html"
+# class FolioListHTMLRenderer(CustomHTMLRenderer):
+#     template_name = "folio/folio_list.html"
+#
+#
+# class FolioDetailHTMLRenderer(CustomHTMLRenderer):
+#     template_name = "folio/folio_detail.html"
 
 
 class FolioList(generics.ListCreateAPIView):
     model = Folio
-    serializer_class = ConcordanceSerializer
-    renderer_classes = (JSONRenderer, JSONPRenderer,
-                        FolioListHTMLRenderer)
+    serializer_class = FolioSerializer
+    renderer_classes = (JSONRenderer, JSONPRenderer)
+                        # FolioListHTMLRenderer)
 
 
 class FolioDetail(generics.RetrieveUpdateDestroyAPIView):
     model = Folio
-    serializer_class = ConcordanceSerializer
-    renderer_classes = (JSONRenderer, JSONPRenderer,
-                        FolioDetailHTMLRenderer)
+    serializer_class = FolioSerializer
+    renderer_classes = (JSONRenderer, JSONPRenderer)
+                        # FolioDetailHTMLRenderer)
