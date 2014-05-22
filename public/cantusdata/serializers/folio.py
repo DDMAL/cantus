12469f3445e9831762b1dd94afdeeafd0601533d
AFFogarty <andrew.f.fogarty@gmail.com>
2014-05-22 14:46:08 -0400

    New: Folios now list their chants in API, and manuscripts list their folios and chants

--- public/cantusdata/serializers/folio.py
+++ public/cantusdata/serializers/folio.py
@@ -5,3 +5,6 @@ from rest_framework import serializers
 class FolioSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Folio
+
+    chant_set = serializers.HyperlinkedRelatedField(many=True, read_only=True,
+                                                 view_name="chant-detail")
