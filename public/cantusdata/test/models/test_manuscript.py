5c786e1a902e36077b46f1823c78357c795e45ff
AFFogarty <andrew.f.fogarty@gmail.com>
2014-05-23 13:41:01 -0400

    New: Test Manuscript.folio_count

--- public/cantusdata/test/models/test_manuscript.py
+++ public/cantusdata/test/models/test_manuscript.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.db import IntegrityError
 from cantusdata.models.manuscript import Manuscript
+from cantusdata.models.folio import Folio
 
 
 class ManuscriptModelTestCase(TestCase):
@@ -8,19 +10,57 @@ class ManuscriptModelTestCase(TestCase):
         Manuscript.objects.create(name="MyName", siglum=u"    67  a# _ 1*",
                                   date="tomorrow", provenance="provigo",
                                   description="A very nice manuscript, if i do"+
-                                              "say so myself!",
-                                  )
+                                              "say so myself!")
+        Manuscript.objects.create(name="NumberTwo", siglum=u"abcde")
 
     def test_folio_count(self):
-        pass
+        """
+        Test that the manuscript folio count is updated correctly.
+        """
+        # No folios
+        first_manuscript = Manuscript.objects.get(name="MyName")
+        self.assertEqual(first_manuscript.folio_count, 0)
+
+        # One folio
+        Folio.objects.create(number="123", manuscript=first_manuscript)
+        self.assertEqual(first_manuscript.folio_count, 1)
+        # Two folios
+        Folio.objects.create(number="456", manuscript=first_manuscript)
+        self.assertEqual(first_manuscript.folio_count, 2)
+
+        # Make sure that a folio from another manuscript doesn't affect count
+        second_manuscript = Manuscript.objects.get(name="NumberTwo")
+        self.assertEqual(second_manuscript.folio_count, 0)
+        Folio.objects.create(number="789", manuscript=second_manuscript)
+        self.assertEqual(second_manuscript.folio_count, 1)
+        self.assertEqual(first_manuscript.folio_count, 2)
+
+        # First deletion
+        Folio.objects.get(number="123").delete()
+        self.assertEqual(first_manuscript.folio_count, 1)
+        # Second deletion
+        Folio.objects.get(number="456").delete()
+        self.assertEqual(first_manuscript.folio_count, 0)
 
     def test_chant_set(self):
         pass
 
     def test_siglum_slug(self):
+        """
+        Test that automatic siglum slug generation works.
+        """
         manuscript = Manuscript.objects.get(name="MyName")
         self.assertEqual(manuscript.siglum_slug, u"67-a-_-1")
         # Now we want to try to change the siglum slug
         manuscript.siglum = u"  ano ther @ attmpt"
         manuscript.save()
         self.assertEqual(manuscript.siglum_slug, "ano-ther-attmpt")
+
+    def test_unique_siglum_slug(self):
+        """
+        Test that the manuscript siglum is unique by asserting that an
+        IntegrityError is raised when we try to create a duplicate manuscript.
+        """
+        with self.assertRaises(IntegrityError):
+            Manuscript.objects.create(name="NumberTwo",
+                                      siglum=u"    67  a# _ 1*")
