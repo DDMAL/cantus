0b211211b74be2257744a7aafdbd6b94d96b0d84
AFFogarty <andrew.f.fogarty@gmail.com>
2014-05-23 16:53:27 -0400

    Fixed: Model test coverage and performance improvements

--- public/cantusdata/test/models/test_folio.py
+++ public/cantusdata/test/models/test_folio.py
@@ -6,29 +6,35 @@ from cantusdata.models.manuscript import Manuscript
 
 class FolioModelTestCase(TestCase):
 
+    manuscript = None
+    folio = None
+
     def setUp(self):
         # Generic folio
-        manuscript = Manuscript.objects.create(siglum=u"ABC - 456")
-        Folio.objects.create(number="123", manuscript=manuscript)
+        self.manuscript = Manuscript.objects.create(siglum=u"ABC - 456",
+                                               name="geoff")
+        self.folio = Folio.objects.create(number="123",
+                                          manuscript=self.manuscript)
+
+    def test_unicode(self):
+        self.assertEqual(self.folio.__unicode__(), u"123 - ABC - 456 - geoff")
 
     def test_chant_count(self):
-        manuscript = Manuscript.objects.get(siglum_slug="abc-456")
-        # Get the folio
-        folio = Folio.objects.get(number="123")
         # Folio has no chants yet
-        self.assertEqual(folio.chant_count, 0)
+        self.assertEqual(self.folio.chant_count, 0)
         # We are going to give it a chant
-        chant = Chant.objects.create(sequence=2, manuscript=manuscript,
-                                     folio=folio)
+        chant = Chant.objects.create(sequence=2, manuscript=self.manuscript,
+                                     folio=self.folio)
         # After saving the chant, the folio should have 1 chant
-        self.assertEqual(folio.chant_count, 1)
+        self.assertEqual(self.folio.chant_count, 1)
         # Now we want to try it again and see if we go to 2
-        second_chant = Chant.objects.create(sequence=5, manuscript=manuscript,
-                                            folio=folio)
+        second_chant = Chant.objects.create(sequence=5,
+                                            manuscript=self.manuscript,
+                                            folio=self.folio)
         # After saving the chant, the folio should have 1 chant
-        self.assertEqual(folio.chant_count, 2)
+        self.assertEqual(self.folio.chant_count, 2)
         # Now we test deletes
         chant.delete()
-        self.assertEqual(folio.chant_count, 1)
+        self.assertEqual(self.folio.chant_count, 1)
         second_chant.delete()
-        self.assertEqual(folio.chant_count, 0)
+        self.assertEqual(self.folio.chant_count, 0)
