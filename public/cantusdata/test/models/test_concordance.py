481643323118f46aece57cfc556726cbcbdd257a
AFFogarty <andrew.f.fogarty@gmail.com>
2014-05-23 16:53:27 -0400

    Fixed: Model test coverage and performance improvements

--- public/cantusdata/test/models/test_concordance.py
+++ public/cantusdata/test/models/test_concordance.py
@@ -13,6 +13,10 @@ class ConcordanceModelTestCase(TestCase):
                                    location="Saturn",
                                    rism_code="QUE - 982")
 
+    def test_unicode(self):
+        concordance = Concordance.objects.get(letter_code="A")
+        self.assertEqual(concordance.__unicode__(), u"A - DDMAL")
+
     def test_citation(self):
         concordance = Concordance.objects.get(letter_code="A")
         self.assertEqual(concordance.citation,
