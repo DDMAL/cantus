63143898c6393919927005a5e38d5a5246dfb4f0
AFFogarty <andrew.f.fogarty@gmail.com>
2014-05-23 17:03:16 -0400

    Fixed: Manuscript tests more efficient

--- public/cantusdata/test/models/test_manuscript.py
+++ public/cantusdata/test/models/test_manuscript.py
@@ -7,78 +7,80 @@ from cantusdata.models.folio import Folio
 
 class ManuscriptModelTestCase(TestCase):
 
+    first_manuscript = None
+    second_manuscript = None
+
     def setUp(self):
-        Manuscript.objects.create(name="MyName", siglum=u"    67  a# _ 1*",
-                                  date="tomorrow", provenance="provigo",
-                                  description="A very nice manuscript...")
-        Manuscript.objects.create(name="NumberTwo", siglum=u"abcde")
+        self.first_manuscript = Manuscript.objects.create(
+            name="MyName", siglum=u"    67  a# _ 1*", date="tomorrow",
+            provenance="provigo", description="A very nice manuscript...")
+        self.second_manuscript = Manuscript.objects.create(name="NumberTwo",
+                                                           siglum=u"abcde")
+
+    def test_unicode(self):
+        self.assertEqual(self.first_manuscript.__unicode__(),
+                         "    67  a# _ 1* - MyName")
 
     def test_folio_count(self):
         """
         Test that the manuscript folio count is updated correctly.
         """
-        first_manuscript = Manuscript.objects.get(name="MyName")
-        second_manuscript = Manuscript.objects.get(name="NumberTwo")
-
         # No folios
-        self.assertEqual(first_manuscript.folio_count, 0)
+        self.assertEqual(self.first_manuscript.folio_count, 0)
         # One folio
-        Folio.objects.create(number="123", manuscript=first_manuscript)
-        self.assertEqual(first_manuscript.folio_count, 1)
+        Folio.objects.create(number="123", manuscript=self.first_manuscript)
+        self.assertEqual(self.first_manuscript.folio_count, 1)
         # Two folios
-        Folio.objects.create(number="456", manuscript=first_manuscript)
-        self.assertEqual(first_manuscript.folio_count, 2)
-
+        Folio.objects.create(number="456", manuscript=self.first_manuscript)
+        self.assertEqual(self.first_manuscript.folio_count, 2)
         # Make sure that a folio from another manuscript doesn't affect count
-        self.assertEqual(second_manuscript.folio_count, 0)
-        Folio.objects.create(number="789", manuscript=second_manuscript)
-        self.assertEqual(second_manuscript.folio_count, 1)
-        self.assertEqual(first_manuscript.folio_count, 2)
-
+        self.assertEqual(self.second_manuscript.folio_count, 0)
+        Folio.objects.create(number="789", manuscript=self.second_manuscript)
+        self.assertEqual(self.second_manuscript.folio_count, 1)
+        self.assertEqual(self.first_manuscript.folio_count, 2)
         # First deletion
         Folio.objects.get(number="123").delete()
-        self.assertEqual(first_manuscript.folio_count, 1)
+        self.assertEqual(self.first_manuscript.folio_count, 1)
         # Second deletion
         Folio.objects.get(number="456").delete()
-        self.assertEqual(first_manuscript.folio_count, 0)
+        self.assertEqual(self.first_manuscript.folio_count, 0)
 
     def test_chant_set(self):
         """
         Test that the manuscript chant set is updated correctly.
         """
-        test_chant_set = set()
-        first_manuscript = Manuscript.objects.get(name="MyName")
-        second_manuscript = Manuscript.objects.get(name="NumberTwo")
-        first_folio = Folio.objects.create(number="f1", manuscript=first_manuscript)
-        second_folio = Folio.objects.create(number="f2", manuscript=second_manuscript)
-
+        first_folio = Folio.objects.create(number="f1",
+                                           manuscript=self.first_manuscript)
+        second_folio = Folio.objects.create(number="f2",
+                                            manuscript=self.second_manuscript)
         # No chants
-        self.assertEqual(set(first_manuscript.chant_set), set())
+        self.assertEqual(set(self.first_manuscript.chant_set), set())
         # One chant
-        first_chant = Chant.objects.create(sequence=1, manuscript=first_manuscript,
-                             folio=first_folio)
-        self.assertEqual(set(first_manuscript.chant_set), {first_chant})
+        first_chant = Chant.objects.create(sequence=1,
+                                           manuscript=self.first_manuscript,
+                                           folio=first_folio)
+        self.assertEqual(set(self.first_manuscript.chant_set), {first_chant})
         # Two chants
-        second_chant = Chant.objects.create(sequence=2, manuscript=first_manuscript,
-                             folio=first_folio)
-        self.assertEqual(set(first_manuscript.chant_set),
+        second_chant = Chant.objects.create(sequence=2,
+                                            manuscript=self.first_manuscript,
+                                            folio=first_folio)
+        self.assertEqual(set(self.first_manuscript.chant_set),
                          {first_chant, second_chant})
-
         # Make sure that a chant from another manuscript doesn't affect set
-        self.assertEqual(set(second_manuscript.chant_set), set())
-        third_chant = Chant.objects.create(sequence=3, manuscript=second_manuscript,
-                             folio=second_folio)
-        self.assertEqual(set(second_manuscript.chant_set),
+        self.assertEqual(set(self.second_manuscript.chant_set), set())
+        third_chant = Chant.objects.create(sequence=3,
+                                           manuscript=self.second_manuscript,
+                                           folio=second_folio)
+        self.assertEqual(set(self.second_manuscript.chant_set),
                          {third_chant})
-        self.assertEqual(set(first_manuscript.chant_set),
+        self.assertEqual(set(self.first_manuscript.chant_set),
                          {second_chant, first_chant})
-
         # First deletion
         first_chant.delete()
-        self.assertEqual(set(first_manuscript.chant_set), {second_chant})
+        self.assertEqual(set(self.first_manuscript.chant_set), {second_chant})
         # Second deletion
         second_chant.delete()
-        self.assertEqual(set(first_manuscript.chant_set), set())
+        self.assertEqual(set(self.first_manuscript.chant_set), set())
 
     def test_siglum_slug(self):
         """
