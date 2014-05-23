b1ed53192c248cbb5831f8fb8614fb88fa967fba
AFFogarty <andrew.f.fogarty@gmail.com>
2014-05-23 16:27:19 -0400

    New: Chant unicode test

--- public/cantusdata/test/models/test_chant.py
+++ public/cantusdata/test/models/test_chant.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
 from cantusdata.models.chant import Chant
+from cantusdata.models.manuscript import Manuscript
+from cantusdata.models.folio import Folio
 
 
 class ChantModelTestCase(TestCase):
 
-    chant = None
-
     def setUp(self):
-        self.chant = Chant()
+        manuscript = Manuscript.objects.create(name="MyName", siglum=u"    67  a# _ 1*",
+                                  date="tomorrow", provenance="provigo",
+                                  description="A very nice manuscript...")
+        folio = Folio.objects.create(number="123", manuscript=manuscript)
+        Chant.objects.create(cantus_id="1234", incipit="5678", sequence="72",
+                             manuscript=manuscript, folio=folio)
+
+    def test_unicode(self):
+        chant = Chant.objects.get(cantus_id="1234")
+        self.assertEqual(chant.__unicode__(), u"1234 - 5678")
