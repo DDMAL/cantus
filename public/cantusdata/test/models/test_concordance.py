4dd0e82dd12ea08d9d705091c91b07b4c12b79fe
AFFogarty <andrew.f.fogarty@gmail.com>
2014-05-26 12:04:10 -0400

    New: 100% code coverage on model tests

--- public/cantusdata/test/models/test_concordance.py
+++ public/cantusdata/test/models/test_concordance.py
@@ -1,11 +1,14 @@
+import copy
 from django.test import TestCase
 from cantusdata.models.concordance import Concordance
 
 
 class ConcordanceModelTestCase(TestCase):
 
+    concordance = None
+
     def setUp(self):
-        Concordance.objects.create(letter_code="A",
+        self.concordance = Concordance.objects.create(letter_code="A",
                                    institution_city="Montreal",
                                    institution_name="DDMAL",
                                    library_manuscript_name="ABC123",
@@ -14,11 +17,25 @@ class ConcordanceModelTestCase(TestCase):
                                    rism_code="QUE - 982")
 
     def test_unicode(self):
-        concordance = Concordance.objects.get(letter_code="A")
-        self.assertEqual(concordance.__unicode__(), u"A - DDMAL")
+        self.assertEqual(self.concordance.__unicode__(), u"A - DDMAL")
 
     def test_citation(self):
-        concordance = Concordance.objects.get(letter_code="A")
-        self.assertEqual(concordance.citation,
+        self.assertEqual(self.concordance.citation,
                              "A  Montreal, DDMAL, ABC123" +
                              " (Today, from Saturn) [RISM: QUE - 982]")
+
+    def test_update_solr(self):
+        """
+        Test that we can update the concordance in Solr.
+        """
+        duplicate_concordance = copy.deepcopy(self.concordance)
+        self.assertFalse(self.concordance is duplicate_concordance)
+        self.assertEqual(self.concordance.institution_city,
+                         duplicate_concordance.institution_city)
+        self.concordance.institution_city = "Toronto"
+        self.concordance.save()
+        self.assertNotEqual(self.concordance.institution_city,
+                            duplicate_concordance.institution_city)
+
+    def tearDown(self):
+        Concordance.objects.all().delete()
