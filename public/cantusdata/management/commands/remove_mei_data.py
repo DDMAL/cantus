2c2567570fff68ae16bd60603e0b770ae5220186
AFFogarty <andrew.f.fogarty@gmail.com>
2014-07-21 15:31:00 -0400

    New: Script to remove a particular manuscripts notation data from Solr

---
+++ public/cantusdata/management/commands/remove_mei_data.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+import solr
+
+class Command(BaseCommand):
+    args = ""
+
+    def handle(self, *args, **kwargs):
+        # Grab the manuscript name
+        manuscript = args[0]
+
+        if manuscript == "salzinnes":
+            siglum = "cdn-hsmu-m2149l4"
+        elif manuscript == "st_gallen_390":
+            siglum = "ch-sgs-390"
+        elif manuscript == "st_gallen_391":
+            siglum = "ch-sgs-391"
+        else:
+            raise Exception("Please provide manuscript name!")
+
+        self.stdout.write("Removing {0} music notation data from Solr...".format(manuscript))
+        solrconn = solr.SolrConnection(settings.SOLR_SERVER)
+        solrconn.delete_query("type:cantusdata_music_notation AND siglum:{0}".format(siglum))
+        solrconn.commit()
+        self.stdout.write("Success.")
