bd362423d12e7b5a3a9c511ab0dd0e6a19d4a3c6
AFFogarty <andrew.f.fogarty@gmail.com>
2014-07-29 14:16:27 -0400

    Test: Expanded folio set tests

--- public/cantusdata/test/views/test_folio_set.py
+++ public/cantusdata/test/views/test_folio_set.py
@@ -1,4 +1,4 @@
-from cantusdata.helpers.solr_result_parsing import remove_unique_id, remove_number
+from cantusdata.helpers.solr_result_parsing import remove_all_solr_metadata
 from rest_framework.test import APITestCase
 from rest_framework import status
 
@@ -19,15 +19,20 @@ class ManuscriptFolioSetViewTestCase(APITestCase):
                           ' "item_id": "1", "type": "cantusdata_folio"}]'
         # We want to remove the version id and unique id because they're always
         # different.
-        self.assertEqual(
-            remove_number
-            (
-                remove_number
-                (
-                    remove_unique_id(response.content),
-                    "_version_"
-                ),
-                "score"
-            ),
-            expected_string
-        )
+        self.assertEqual(remove_all_solr_metadata(response.content),
+                         expected_string)
+
+    def test_get_specific_folio(self):
+        response = self.client.get("/folio-set/manuscript/3/123/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        expected_string = '{"manuscript_id": 3, "number": "123",' \
+                          ' "item_id": "1", "type": "cantusdata_folio"}'
+        # Empty response is just square brackets
+        self.assertEqual(remove_all_solr_metadata(response.content),
+                         expected_string)
+
+    def test_get_empty_folio(self):
+        response = self.client.get("/folio-set/manuscript/3/66666666666/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Empty response is just square brackets
+        self.assertEqual(response.content, "[]")
