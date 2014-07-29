1ccb6d74a08a12574520b8723e67af7a9aa913b5
AFFogarty <andrew.f.fogarty@gmail.com>
2014-07-29 14:33:05 -0400

    Test: Chant set view now has 100% coverage

--- public/cantusdata/test/views/test_chant_set.py
+++ public/cantusdata/test/views/test_chant_set.py
@@ -1,4 +1,4 @@
-from cantusdata.helpers.solr_result_parsing import remove_unique_id, remove_number
+from cantusdata.helpers.solr_result_parsing import remove_all_solr_metadata
 from rest_framework.test import APITestCase
 from rest_framework import status
 
@@ -19,8 +19,7 @@ class FolioChantSetViewTestCase(APITestCase):
                           ' "differentia": "", "marginalia": "",' \
                           ' "cantus_id": "1234",' \
                           ' "folio": "123", "manuscript": "ABC - 456",' \
-                          ' "folio_id": 1, "score": 14.888019,' \
-                          ' "type": "cantusdata_chant",' \
+                          ' "folio_id": 1, "type": "cantusdata_chant",' \
                           ' "manuscript_name_hidden": "geoff",' \
                           ' "incipit": "5678", "volpiano": "",' \
                           ' "genre": "", "manuscript_id": 3, "full_text": "",' \
@@ -28,8 +27,7 @@ class FolioChantSetViewTestCase(APITestCase):
                           ' "position": ""}]'
         # We want to remove the version id and unique id because they're always
         # different.
-        self.assertEqual(remove_number(remove_unique_id(response.content),
-                                       "_version_"),
+        self.assertEqual(remove_all_solr_metadata(response.content),
                          expected_string)
 
 
@@ -58,15 +56,12 @@ class ManuscriptChantSetTestCase(APITestCase):
                           ' "position": ""}]'
         # We want to remove the version id and unique id because they're always
         # different.
-        self.assertEqual(
-            remove_number
-            (
-                remove_number
-                (
-                    remove_unique_id(response.content),
-                    "_version_"
-                ),
-                "score"
-            ),
-            expected_string
-        )
+        self.assertEqual(remove_all_solr_metadata(response.content),
+                         expected_string)
+
+    def test_get_empty_chant(self):
+        response = self.client.get("/chant-set/manuscript/3/page-2/")
+        # Test that we get a response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Empty response is just square brackets
+        self.assertEqual(response.content, "[]")
