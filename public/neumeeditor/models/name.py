c4b5360084857a4f9c625695d477b05e47410fb7
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-07 11:24:02 -0400

    New: Name model

---
+++ public/neumeeditor/models/name.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Name(models.Model):
+    class Meta:
+        app_label = "neumeeditor"
+        ordering = ['string']
+
+    string = models.CharField(max_length=128, blank=False, null=False,
+                              unique=True)
+    glyph = models.ForeignKey("neumeeditor.Glyph", null=True)
+
+    def __unicode__(self):
+        return u"{0}".format(self.string)
