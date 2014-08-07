8d47026cb64f308afd40087f1187ca013d1a446b
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-07 15:25:24 -0400

    New: Script to import Glyphs

---
+++ public/neumeeditor/management/commands/import_neume_data.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.utils.text import slugify
+
+import csv
+from neumeeditor.models.style import Style
+from neumeeditor.models.name import Name
+from neumeeditor.models.glyph import Glyph
+
+
+class Command(BaseCommand):
+    args = ""
+
+    def handle(self, *args, **kwargs):
+        self.import_csv_file("ccnames.csv", "Salzinnes")
+
+    def import_csv_file(self, file_name, style_name):
+        csv_file = csv.DictReader(open("data_dumps/glyphs/" + str(file_name)))
+        # Get the style object
+        style = Style.objects.get(name=str(style_name))
+        index = 0
+        for row in csv_file:
+            glyph = Glyph.objects.create(style=style)
+            Name.objects.create(string=str(row["name"]),
+                                short_code=str(row["short_code"]),
+                                glyph=glyph)
+            index += 1
+
+        self.stdout.write(
+            u"Successfully imported {0} glyphs into database.".format(index))
