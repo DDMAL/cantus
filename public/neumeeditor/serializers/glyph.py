64cf68042d7bccfe3d950a2c86354a725770fb38
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-07 16:56:23 -0400

    New: Glyph is serialized with Style string and name_set

--- public/neumeeditor/serializers/glyph.py
+++ public/neumeeditor/serializers/glyph.py
@@ -4,6 +4,9 @@ from rest_framework import serializers
 
 class GlyphSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.Field()
+    style = serializers.RelatedField(many=False, read_only=True)
+    name_set = serializers.HyperlinkedRelatedField(many=True, read_only=True,
+                                                    view_name="name-detail")
 
     class Meta:
         model = Glyph
