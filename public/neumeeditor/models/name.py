7d8f95e2da59f9b0cfdf16b9f6a49a9d778ff01e
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-07 15:25:54 -0400

    New: Names also include a short code

--- public/neumeeditor/models/name.py
+++ public/neumeeditor/models/name.py
@@ -8,6 +8,8 @@ class Name(models.Model):
 
     string = models.CharField(max_length=128, blank=False, null=False,
                               unique=True)
+    short_code = models.CharField(max_length=128, blank=False, null=False,
+                                  unique=True)
     glyph = models.ForeignKey("neumeeditor.Glyph", null=True)
 
     def __unicode__(self):
