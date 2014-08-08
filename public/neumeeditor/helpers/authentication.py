2aec07437bf71e9ba610e33e1fb1fe3296406925
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-08 16:46:33 -0400

    New: Auth token max age is now a global setting

--- public/neumeeditor/helpers/authentication.py
+++ public/neumeeditor/helpers/authentication.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from cantusdata import settings
 from django.contrib.auth import tokens
 from django.utils.datetime_safe import datetime
 from rest_framework.authentication import TokenAuthentication
@@ -7,9 +8,6 @@ import pytz
 
 
 class ExpiringTokenAuthentication(TokenAuthentication):
-
-    max_age_days = 3
-
     def authenticate_credentials(self, key):
         try:
             token = self.model.objects.get(key=key)
@@ -17,9 +15,7 @@ class ExpiringTokenAuthentication(TokenAuthentication):
             raise exceptions.AuthenticationFailed('Invalid token')
         if not token.user.is_active:
             raise exceptions.AuthenticationFailed('User inactive or deleted')
-
         utc_now = datetime.utcnow().replace(tzinfo=pytz.utc)
-
-        if token.created < utc_now - timedelta(days=self.max_age_days):
+        if token.created < utc_now - timedelta(days=settings.MAX_TOKEN_AGE_DAYS):
             raise exceptions.AuthenticationFailed('Token has expired')
         return token.user, tokens
