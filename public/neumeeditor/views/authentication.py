547a993672e07dd4e17814d304440136258107f5
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-08 15:36:24 -0400

    New: Authorization tokens can be deleted
    This way, users can be "logged out" globally.

--- public/neumeeditor/views/authentication.py
+++ public/neumeeditor/views/authentication.py
@@ -1,8 +1,9 @@
 from django.utils.datetime_safe import datetime
 from rest_framework import status
-from rest_framework.authtoken.models import Token
+from rest_framework.authentication import get_authorization_header
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 
 class ObtainExpiringAuthToken(ObtainAuthToken):
@@ -15,3 +16,23 @@ class ObtainExpiringAuthToken(ObtainAuthToken):
                 token.save()
             return Response({'token': token.key})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        auth = get_authorization_header(request).split()
+        if not auth or auth[0].lower() != b'token':
+            return Response({"status": "No token provided."},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        if len(auth) == 1:
+            msg = 'Invalid token header. No credentials provided.'
+            return Response({"status": msg}, status=status.HTTP_400_BAD_REQUEST)
+        elif len(auth) > 2:
+            msg = 'Invalid token header. Token string should not contain spaces.'
+            return Response({"status": msg}, status=status.HTTP_400_BAD_REQUEST)
+        # The token was provided, so we are going to delete it
+        try:
+            token = Token.objects.get(key=auth[1])
+            token.delete()
+            return Response({'status': "Token deleted.".format(auth[1])})
+        except Token.DoesNotExist:
+            return Response({"status": "Token does not exist."},
+                            status=status.HTTP_401_UNAUTHORIZED)
