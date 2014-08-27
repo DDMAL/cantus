1cf257f451dae88f23cf3f39f106265fd8081b00
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-27 14:47:26 -0400

    New: Image view

---
+++ public/neumeeditor/views/image.py
@@ -0,0 +1,25 @@
+from neumeeditor.helpers.authentication import ExpiringTokenAuthentication
+from neumeeditor.models.image import Image
+from neumeeditor.serializers.image import ImageSerializer
+from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer, JSONPRenderer
+
+
+class NameList(generics.ListCreateAPIView):
+    model = Image
+    serializer_class = ImageSerializer
+    renderer_classes = (JSONRenderer, JSONPRenderer)
+    authentication_classes = (ExpiringTokenAuthentication,
+                              SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+
+class NameDetail(generics.RetrieveUpdateDestroyAPIView):
+    model = Image
+    serializer_class = ImageSerializer
+    renderer_classes = (JSONRenderer, JSONPRenderer)
+    authentication_classes = (ExpiringTokenAuthentication,
+                              SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
