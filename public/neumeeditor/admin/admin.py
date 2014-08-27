a8948708f736cb0d598048946b1efc11d7fe443e
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-27 14:43:37 -0400

    New: Image model admin

--- public/neumeeditor/admin/admin.py
+++ public/neumeeditor/admin/admin.py
@@ -1,18 +1,22 @@
 from django.contrib import admin
 from neumeeditor.models import Name
 from neumeeditor.models.glyph import Glyph
+from neumeeditor.models.image import Image
 from neumeeditor.models.style import Style
 
 
 class NameInline(admin.TabularInline):
     model = Name
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
 
 class GlyphAdmin(admin.ModelAdmin):
     inlines = [
-        NameInline
+        NameInline,
+        ImageInline
     ]
-
 
 class StyleAdmin(admin.ModelAdmin):
     pass
