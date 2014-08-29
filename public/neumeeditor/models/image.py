938294a5d88652e51b367ee5681ec85438b64c88
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-29 17:09:39 -0400

    New: Images are stored with the hashed storage system

--- public/neumeeditor/models/image.py
+++ public/neumeeditor/models/image.py
@@ -1,9 +1,22 @@
+import hashlib
 from django.db import models
+from neumeeditor.helpers.storage.media_file_system import media_file_name, \
+    MediaFileSystemStorage
 
 
 class Image(models.Model):
     class Meta:
         app_label = "neumeeditor"
 
-    image_file = models.ImageField(null=True)
+    image_file = models.ImageField(null=True, upload_to=media_file_name,
+                                   storage=MediaFileSystemStorage())
     glyph = models.ForeignKey("neumeeditor.Glyph", null=True)
+    md5sum = models.CharField(null=True, max_length=36)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # file is new
+            md5 = hashlib.md5()
+            for chunk in self.image_file.chunks():
+                md5.update(chunk)
+            self.md5sum = md5.hexdigest()
+        super(Image, self).save(*args, **kwargs)
