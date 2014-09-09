19fa4fb238f06df31a89dd06d822865632943135
AFFogarty <andrew.f.fogarty@gmail.com>
2014-09-09 10:58:34 -0400

    New: Name model uses a custom field for short codes
    short_code values are automatically filtered to remove unwanted characters

--- public/neumeeditor/models/name.py
+++ public/neumeeditor/models/name.py
@@ -1,4 +1,5 @@
 from django.db import models
+from neumeeditor.models.fields.short_code_field import ShortCodeField
 
 
 class Name(models.Model):
@@ -8,9 +9,9 @@ class Name(models.Model):
 
     string = models.CharField(max_length=128, blank=False, null=False,
                               unique=True)
-    short_code = models.CharField(max_length=128, blank=False, null=False,
+    short_code = ShortCodeField(max_length=128, blank=False, null=False,
                                   unique=True)
-    glyph = models.ForeignKey("neumeeditor.Glyph", null=True)
+    glyph = models.ForeignKey("neumeeditor.Glyph", null=True)  
 
     def __unicode__(self):
         return u"{0}".format(self.string)
