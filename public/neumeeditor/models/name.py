b6d3def01e1710e87796e1f79b3eaf42f877ca50
AFFogarty <andrew.f.fogarty@gmail.com>
2014-09-11 10:18:23 -0400

    Fixed: Better whitespace on Name model

--- public/neumeeditor/models/name.py
+++ public/neumeeditor/models/name.py
@@ -10,8 +10,8 @@ class Name(models.Model):
     string = models.CharField(max_length=128, blank=False, null=False,
                               unique=True)
     short_code = ShortCodeField(max_length=128, blank=False, null=False,
-                                  unique=True)
-    glyph = models.ForeignKey("neumeeditor.Glyph", null=True)  
+                                unique=True)
+    glyph = models.ForeignKey("neumeeditor.Glyph", null=True)
 
     def __unicode__(self):
         return u"{0}".format(self.string)
