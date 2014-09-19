ac1f0ed4f3f6f2a775aa18b7874845f54ab806f8
AFFogarty <andrew.f.fogarty@gmail.com>
2014-09-19 14:18:24 -0400

    New: Glyph names are serialized directly

--- public/neumeeditor/serializers/glyph.py
+++ public/neumeeditor/serializers/glyph.py
@@ -1,12 +1,19 @@
 from neumeeditor.models.glyph import Glyph
 from rest_framework import serializers
+from neumeeditor.models.name import Name
+
+class GlyphNameSetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Name
+        fields = ('string', 'short_code', 'glyph')
 
 
 class GlyphSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.Field()
     style = serializers.RelatedField(many=False, read_only=True)
-    name_set = serializers.HyperlinkedRelatedField(many=True, read_only=True,
-                                                    view_name="name-detail")
+    # name_set = serializers.HyperlinkedRelatedField(many=True, read_only=True,
+    #                                                 view_name="name-detail")
+    name_set = GlyphNameSetSerializer()
     image_set = serializers.HyperlinkedRelatedField(many=True, read_only=True,
                                                    view_name="image-detail")
 
