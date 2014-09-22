04c201c8a219da7515d6232c4f12ba2ee2190f45
AFFogarty <andrew.f.fogarty@gmail.com>
2014-09-22 18:48:13 -0400

    New: Images serialized with absolute URLs

--- public/neumeeditor/serializers/image.py
+++ public/neumeeditor/serializers/image.py
@@ -1,9 +1,18 @@
 from neumeeditor.models.image import Image
 from rest_framework import serializers
+from django.conf import settings
+import os
 
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.Field()
+    image_file = serializers.SerializerMethodField("retrieve_image_file")
 
     class Meta:
         model = Image
+
+    def retrieve_image_file(self, obj):
+        request = self.context.get('request', None)
+        path = os.path.relpath(obj.image_file.path, settings.MEDIA_ROOT)
+        url = request.build_absolute_uri(os.path.join(settings.MEDIA_URL_NEUMEEDITOR, path))
+        return url
