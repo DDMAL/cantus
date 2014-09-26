4776d1bd2618d2df8c73722c44ad41981f5b02dd
AFFogarty <andrew.f.fogarty@gmail.com>
2014-09-26 14:10:40 -0400

    Fixed: Serialize images relatively

--- public/neumeeditor/serializers/image.py
+++ public/neumeeditor/serializers/image.py
@@ -6,13 +6,13 @@ import os
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.Field()
-    image_file = serializers.SerializerMethodField("retrieve_image_file")
+    # image_file = serializers.SerializerMethodField("retrieve_image_file")
 
     class Meta:
         model = Image
 
-    def retrieve_image_file(self, obj):
-        request = self.context.get('request', None)
-        path = os.path.relpath(obj.image_file.path, settings.MEDIA_ROOT)
-        url = request.build_absolute_uri(os.path.join(settings.MEDIA_URL_NEUMEEDITOR, path))
-        return url
+    # def retrieve_image_file(self, obj):
+        # request = self.context.get('request', None)
+        # path = os.path.relpath(obj.image_file.path, settings.MEDIA_ROOT)
+        # url = request.build_absolute_uri(os.path.join(settings.MEDIA_URL_NEUMEEDITOR, path))
+        # return url
