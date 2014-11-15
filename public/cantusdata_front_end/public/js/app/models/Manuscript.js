69fba8534dd5ba17a5ec24b2b90651db8d115953
AFFogarty <andrew.f.fogarty@gmail.com>
2014-11-14 19:55:03 -0500

    Fixed: Manuscript has defaults

--- public/cantusdata_front_end/public/js/app/models/Manuscript.js
+++ public/cantusdata_front_end/public/js/app/models/Manuscript.js
@@ -7,20 +7,16 @@ define(["jquery", "backbone", "models/CantusAbstractModel"],
 
         return CantusAbstractModel.extend
         ({
-            //defaults: function()
-            //{
-            //    return {
-            //        url: "#",
-            //        name: "Test Name",
-            //        siglum: "Test Siglum",
-            //        siglum_slug: "#",
-            //        date: "Tomorrow",
-            //        provenance: "Test provenance",
-            //        description: "This is a nice manuscript...",
-            //        chant_count: 5
-            //    };
-            //}
+            defaults:  {
+                    url: "#",
+                    name: "Test Name",
+                    siglum: "Test Siglum",
+                    siglum_slug: "#",
+                    date: "Tomorrow",
+                    provenance: "Test provenance",
+                    description: "This is a nice manuscript...",
+                    chant_count: 5
+            }
         });
     }
-
 );
