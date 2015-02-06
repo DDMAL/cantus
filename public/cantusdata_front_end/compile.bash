ff039482a5095c15382dbf449bcd1489427590d3
AFFogarty <andrew.f.fogarty@gmail.com>
2015-02-06 09:36:00 -0500

    New: Templates get auto-assembled when you run the Cantus front end build script
    Fixes #99

--- public/cantusdata_front_end/compile.bash
+++ public/cantusdata_front_end/compile.bash
@@ -1,5 +1,17 @@
 #!/bin/sh
 
 # Compile
-grunt
-# Copy / paste the output
+grunt --force
+
+# Assemble the templates
+cd public/template-assembler/
+python build-template.py
+cd ../../
+# Copy / paste the template
+rm ../cantusdata/templates/require.html
+cp public/template-assembler/build/index.html ../cantusdata/templates/require.html
+echo "Copied Template"
+
+rm -rf ../cantusdata/static/js
+cp -R public/js ../cantusdata/static/js
+echo "Copied JavaScript"
