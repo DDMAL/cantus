045258fcc37399e4fc6d911878e07e1c8dcc4a88
AFFogarty <andrew.f.fogarty@gmail.com>
2015-02-06 12:38:34 -0500

    Fixed: Image serializer gives relative urls

--- public/neumeeditor/serializers/image.py
+++ public/neumeeditor/serializers/image.py
@@ -8,6 +8,7 @@ import os
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
     # image_file = serializers.SerializerMethodField("retrieve_image_file")
+    image_file = serializers.ImageField(allow_empty_file=False, use_url=False)
     glyph = serializers.HyperlinkedRelatedField(view_name='glyph-detail',
                                                 queryset=Glyph.objects.all())
 
@@ -15,7 +16,7 @@ class ImageSerializer(serializers.HyperlinkedModelSerializer):
         model = Image
 
     # def retrieve_image_file(self, obj):
-        # request = self.context.get('request', None)
-        # path = os.path.relpath(obj.image_file.path, settings.MEDIA_ROOT)
-        # url = request.build_absolute_uri(os.path.join(settings.MEDIA_URL_NEUMEEDITOR, path))
-        # return url
+    #     request = self.context.get('request', None)
+    #     path = os.path.relpath(obj.image_file.path, settings.MEDIA_ROOT)
+    #     url = request.build_absolute_uri(os.path.join(settings.MEDIA_URL_NEUMEEDITOR, path))
+    #     return url
