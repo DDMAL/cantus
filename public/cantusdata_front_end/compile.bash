f8e562bda5a9ae933db7d269cb0e9f509d168131
AFFogarty <andrew.f.fogarty@gmail.com>
2015-02-25 14:55:32 -0500

    New: Front End compiler script has production mode

--- public/cantusdata_front_end/compile.bash
+++ public/cantusdata_front_end/compile.bash
@@ -13,5 +13,20 @@ cp public/template-assembler/build/index.html ../cantusdata/templates/require.ht
 echo "Copied Template"
 
 rm -rf ../cantusdata/static/js
-cp -R public/js ../cantusdata/static/js
+
+if [ "$1" = "prod" ]; then
+    # We are compiling for production, so only copy minified code
+    echo "Compiling minified code for production."
+    # Make an empty JS dir
+    mkdir -p ../cantusdata/static/js/app/init/
+    mkdir ../cantusdata/static/js/app/config/
+    mkdir -p ../cantusdata/static/js/libs/
+    cp public/js/app/init/DesktopInit.min.js ../cantusdata/static/js/app/init/DesktopInit.min.js
+    cp public/js/app/config/config.js ../cantusdata/static/js/app/config/config.js
+    cp public/js/libs/require.js ../cantusdata/static/js/libs/require.js
+    # Copy just the minified site
+else
+    # Not production, so copy unminified code.
+    cp -R public/js ../cantusdata/static/js
+fi
 echo "Copied JavaScript"
