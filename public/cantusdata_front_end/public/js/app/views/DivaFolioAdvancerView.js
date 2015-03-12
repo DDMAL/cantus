f8482fba3bdb4eec20f5f62b5576dbde78828aca
AFFogarty <andrew.f.fogarty@gmail.com>
2015-03-12 18:39:31 -0400

    New: A widget with buttons to change the manuscript page
    Refs #143

---
+++ public/cantusdata_front_end/public/js/app/views/DivaFolioAdvancerView.js
@@ -0,0 +1,53 @@
+define( ['App', 'marionette', 'jquery'],
+function(App, Marionette, $) {
+
+"use strict";
+
+/**
+ * A widget with buttons that controls
+ */
+return Marionette.ItemView.extend
+({
+    template: "#diva-folio-advancer-template",
+    tagName: "nav",
+
+    ui: {
+        nextButton: "[name='next']",
+        previousButton: "[name='previous']"
+    },
+
+    events: {
+        "click @ui.nextButton": "nextButtonCallbackHandler",
+        "click @ui.previousButton": "previousButtonCallbackHandler"
+    },
+
+    /**
+     * Get the stored Diva data.
+     *
+     * @returns {*|jQuery}
+     */
+    getDivaData: function() {
+        return $("#diva-wrapper").data('diva');
+    },
+
+    nextButtonCallbackHandler: function()
+    {
+        // We use divaData to call the diva functions
+        var divaData = this.getDivaData();
+        // Grab the current page
+        var currentPageIndex = divaData.getCurrentPageIndex();
+        // Tell Diva to go to the next page
+        divaData.gotoPageByIndex(currentPageIndex + 1);
+    },
+
+    previousButtonCallbackHandler: function()
+    {
+        // We use divaData to call the diva functions
+        var divaData = this.getDivaData();
+        // Grab the current page
+        var currentPageIndex = divaData.getCurrentPageIndex();
+        // Tell Diva to go to the previous page
+        divaData.gotoPageByIndex(currentPageIndex - 1);
+    }
+});
+});
