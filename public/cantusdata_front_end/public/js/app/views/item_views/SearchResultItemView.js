ae1a7a7a07ef632243bca6df84e2a074d6356e11
AFFogarty <andrew.f.fogarty@gmail.com>
2015-03-16 09:06:42 -0400

    New: SearchResultItemView serializes a lot more data

--- public/cantusdata_front_end/public/js/app/views/item_views/SearchResultItemView.js
+++ public/cantusdata_front_end/public/js/app/views/item_views/SearchResultItemView.js
@@ -1,5 +1,5 @@
-define( ['App', 'backbone', 'marionette'],
-    function(App, Backbone, Marionette, template) {
+define( ['App', 'marionette'],
+    function(App, Marionette) {
 
         "use strict";
 
@@ -10,13 +10,31 @@ define( ['App', 'backbone', 'marionette'],
             template: "#search-result-item-template",
             tagName: 'table class="table table-striped"',
 
-            modelEvents: {
+            /**
+             * The type of search.  "all fields", "volpiano", etc.
+             */
+            searchField: undefined,
+
+            showManuscriptName: false,
+
+            modelEvents:
+            {
                 "change": "render"
+            },
+
+            initialize: function(model, searchField)
+            {
+                this.model = model;
+                this.searchField = searchField;
             },
 
             serializeData: function()
             {
                 return {
+                    query: this.model.getQueryWithoutManuscript(),
+                    numFound: this.model.get("numFound"),
+                    searchType: this.searchField,
+                    showManuscriptName: this.showManuscriptName,
                     results: this.model.getFormattedData()
                 };
             }
