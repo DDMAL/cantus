866e04d10125e4ee2c60f499fa017bae3c9e36f2
AFFogarty <andrew.f.fogarty@gmail.com>
2015-03-16 15:42:58 -0400

    New: Background highlights on volpiano search results
    Fixes #141

--- public/cantusdata_front_end/public/js/app/models/SearchResult.js
+++ public/cantusdata_front_end/public/js/app/models/SearchResult.js
@@ -74,46 +74,56 @@ define(["jquery", "backbone", "config/GlobalVars"],
             {
                 var output = [];
 
-                _.each(this.get("results"), function(current)
+                //_.each(this.get("results"), function(current)
+
+                var results = this.get("results");
+
+                console.log("Results:", results);
+
+                if (results !== undefined)
                 {
-                    var newElement = {};
-                    // Remove "cantusdata_" from the type string
-                    newElement.model = current.type.split("_")[1];
-                    newElement.name = current.Name;
-
-                    // Figure out what the name is based on the model in question
-                    switch(newElement.model)
+                    for (var i = 0; i < results.length; i++)
                     {
-                        case "manuscript":
-                            newElement.name = current.name;
-                            // Build the url
-                            newElement.url = "/" + newElement.model + "/" + current.item_id + "/";
-                            break;
+                        var newElement = {};
+                        // Remove "cantusdata_" from the type string
+                        newElement.model = results[i].type.split("_")[1];
+                        newElement.name = results[i].Name;
 
-                        case "chant":
-                            newElement.name = current.incipit;
-                            // Build the url
-                            // We have stored the manuscript name in Solr
-                            newElement.manuscript = current.manuscript_name_hidden;
-                            newElement.folio = current.folio;
-                            newElement.volpiano = current.volpiano;
-                            newElement.url = "/manuscript/" + current.manuscript_id + "/?folio=" + current.folio + "&chant=" + current.sequence;
-                            break;
+                        // Figure out what the name is based on the model in question
+                        switch(newElement.model)
+                        {
+                            case "manuscript":
+                                newElement.name = results[i].name;
+                                // Build the url
+                                newElement.url = "/" + newElement.model + "/" + results[i].item_id + "/";
+                                break;
 
-                        case "concordance":
-                            newElement.name = current.name;
-                            // Build the url
-                            newElement.url = "/" + newElement.model + "/" + current.item_id + "/";
-                            break;
+                            case "chant":
+                                newElement.name = results[i].incipit;
+                                // Build the url
+                                // We have stored the manuscript name in Solr
+                                newElement.manuscript = results[i].manuscript_name_hidden;
+                                newElement.folio = results[i].folio;
+                                newElement.volpiano = this.highlightVolpianoResult(results[i].volpiano);
+                                newElement.url = "/manuscript/" + results[i].manuscript_id + "/?folio=" + results[i].folio + "&chant=" + results[i].sequence;
+                                break;
 
-                        case "folio":
-                            newElement.name = current.name;
-                            // Build the url
-                            newElement.url = "/" + newElement.model + "/" + current.item_id + "/";
-                            break;
+                            case "concordance":
+                                newElement.name = results[i].name;
+                                // Build the url
+                                newElement.url = "/" + newElement.model + "/" + results[i].item_id + "/";
+                                break;
+
+                            case "folio":
+                                newElement.name = results[i].name;
+                                // Build the url
+                                newElement.url = "/" + newElement.model + "/" + results[i].item_id + "/";
+                                break;
+                        }
+                        output.push(newElement);
                     }
-                    output.push(newElement);
-                });
+                }
+
                 return output;
             },
 
@@ -125,6 +135,67 @@ define(["jquery", "backbone", "config/GlobalVars"],
                 return {
                     results: []
                 };
+            },
+
+            /**
+             *  Take a volpiano result string and highlight the substrings
+             *  that are part of the query.
+             *
+             * @param result volpiano result string
+             * @returns {string} highlighted string
+             */
+            highlightVolpianoResult: function(result)
+            {
+                // Try to match the regex first
+                var regexMatch = this.get("query").match(/volpiano: ?\(\"([^\"]*)\"\)/);
+
+                // Handle the case where there is no volpiano query
+                if (regexMatch === null || regexMatch === undefined)
+                    return result;
+
+                // The query string will always be the second value in the array
+                var rawVolpianoQuery = regexMatch[1];
+
+                // Format the volpiano as a regex with optional dashes
+                var regex = this.formatVolpianoWithDashesRegex(rawVolpianoQuery);
+                // Grab all matches from that regex
+                var regexMatches = result.match(regex);
+
+                // Highlight the matches with the proper span tag
+                for (var i = 0; i < regexMatches.length; i++)
+                {
+                    // Add the span tag
+                    result = result.replace(regexMatches[i], '<span class="bg-info">' + regexMatches[i] + '</span>');
+                }
+
+                return result;
+            },
+
+            /**
+             * Create a RegEx matching volpiano with optional dashes from a string
+             * representing a volpiano search query.
+             *
+             * So, "abc" becomes a-*b-*c-* etc.
+             *
+             * @param volpianoWithoutDashes string representing the volpiano without dashes
+             * @returns {RegExp}
+             */
+            formatVolpianoWithDashesRegex: function(volpianoWithoutDashes)
+            {
+                // Empty string that we will fill up
+                var outputAsString = "";
+
+                for (var i = 0; i < volpianoWithoutDashes.length; i++)
+                {
+                    // Add the char at i
+                    outputAsString += volpianoWithoutDashes.charAt(i);
+                    // Add the optional dashes
+                    outputAsString += "-*";
+                }
+
+                // Now we have a string representing a good regex, so we must
+                // create an actual regex object
+                return new RegExp(outputAsString, "g");
             }
         });
     }
