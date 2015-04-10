8f6415a65c79b887e8d98f0e8d3aefed82d672a0
AFFogarty <andrew.f.fogarty@gmail.com>
2015-04-10 14:05:22 -0400

    Fixed: Unused import

--- public/neumeeditor/urls.py
+++ public/neumeeditor/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.conf import settings
 from django.conf.urls.static import static
 from neumeeditor.views.authentication import ObtainExpiringAuthToken
