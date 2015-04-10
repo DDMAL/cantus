86b12d69c3653bacb628fc6449739b1304ce163c
AFFogarty <andrew.f.fogarty@gmail.com>
2015-04-10 14:06:16 -0400

    New: Nomenclature Model
    Refs #156

---
+++ public/neumeeditor/models/nomenclature.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Nomenclature(models.Model):
+    class Meta:
+        app_label = "neumeeditor"
+        ordering = ['name']
+
+    name = models.CharField(max_length=128, blank=False, null=False,
+                            unique=True)
+
+    def __unicode__(self):
+        return u"{0}".format(self.name)
