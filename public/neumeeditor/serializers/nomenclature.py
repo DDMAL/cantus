3a83249fb31ebe2365ea393e5f2abe1d49990b3e
AFFogarty <andrew.f.fogarty@gmail.com>
2015-04-29 16:25:07 -0400

    Fixed: Include nomenclature ID

--- public/neumeeditor/serializers/nomenclature.py
+++ public/neumeeditor/serializers/nomenclature.py
@@ -3,5 +3,7 @@ from rest_framework import serializers
 
 
 class NomenclatureSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Nomenclature
