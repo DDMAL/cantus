34a235f538e142effef2cd769c34377d913b7d08
AFFogarty <andrew.f.fogarty@gmail.com>
2015-05-04 13:45:26 -0400

    New: Glyph short codes have proper underscores now

--- public/neumeeditor/models/fields/short_code_field.py
+++ public/neumeeditor/models/fields/short_code_field.py
@@ -4,11 +4,15 @@ from django.db import models
 
 class ShortCodeField(models.CharField):
     unacceptable_chars = "[^a-z0-9\._]"
+    duplicate_spaces = "\ +"
     description = "A short string representing a glyph name"
 
     def pre_save(self, model_instance, add):
         """
         We want to filter-out the undesirable characters.
         """
-        model_instance.short_code = re.sub(self.unacceptable_chars, '', model_instance.short_code.lower())
+        # Turn spaces into single dashes
+        new_code = re.sub(self.duplicate_spaces, '_', model_instance.short_code.strip().lower())
+        # Filter out everything bad
+        model_instance.short_code = re.sub(self.unacceptable_chars, '', new_code)
         return model_instance.short_code
