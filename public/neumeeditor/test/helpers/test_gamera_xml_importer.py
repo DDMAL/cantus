34c49c32cae09d6a728a159cc4cc0da820c26b2e
AFFogarty <andrew.f.fogarty@gmail.com>
2015-05-06 17:44:46 -0400

    New: A run-length decoder

---
+++ public/neumeeditor/test/helpers/test_gamera_xml_importer.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from cantusdata.settings import MEDIA_ROOT
+from neumeeditor.helpers.gamera_xml_importer import RunLengthImage
+
+
+class RunLengthImageTestCase(TestCase):
+    def test_get_location_of_runlength(self):
+        rli = RunLengthImage(3, 3, "5 4")
+        self.assertEquals(rli.get_location_of_runlength(0), (0, 0))
+        self.assertEquals(rli.get_location_of_runlength(1), (1, 0))
+        self.assertEquals(rli.get_location_of_runlength(2), (2, 0))
+        self.assertEquals(rli.get_location_of_runlength(3), (0, 1))
+        self.assertEquals(rli.get_location_of_runlength(4), (1, 1))
+        self.assertEquals(rli.get_location_of_runlength(5), (2, 1))
+        self.assertEquals(rli.get_location_of_runlength(6), (0, 2))
+        self.assertEquals(rli.get_location_of_runlength(7), (1, 2))
+        self.assertEquals(rli.get_location_of_runlength(8), (2, 2))
+
+    def test_get_image(self):
+        rl = "52 4 52 7 50 8 48 11 46 11 46 12 45 12 44 12 45 13 44 13" \
+             " 43 14 44 13 44 11 45 9 48 9 47 10 47 9 48 9 48 9 48 9 48" \
+             " 9 48 8 49 9 48 9 47 8 1 1 47 8 48 9 48 9 48 10 47 10 47 " \
+             "10 47 10 48 10 46 11 46 11 46 11 46 11 46 11 46 11 46 10" \
+             " 47 11 46 11 47 9 48 9 49 9 48 9 49 9 48 6 51 5 53 4 54 " \
+             "3 55 2 56 0 "
+
+        rli = RunLengthImage(58, 52, rl)
+        image = rli.get_image()
+        image.save(MEDIA_ROOT + "test.png")
