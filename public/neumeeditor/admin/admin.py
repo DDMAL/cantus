e7d523bf68b9ec3a007e6ca77064800fce7de655
AFFogarty <andrew.f.fogarty@gmail.com>
2015-05-06 15:56:36 -0400

    New: Can update NameNomenclatureMemberships from admin

--- public/neumeeditor/admin/admin.py
+++ public/neumeeditor/admin/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from neumeeditor.models import Name
 from neumeeditor.models.glyph import Glyph
 from neumeeditor.models.image import Image
+from neumeeditor.models.name_nomenclature_membership import \
+    NameNomenclatureMembership
 from neumeeditor.models.style import Style
 from neumeeditor.models.nomenclature import Nomenclature
 
@@ -33,7 +35,12 @@ class NomenclatureAdmin(admin.ModelAdmin):
     pass
 
 
+class NameNomenclatureMembershipAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Glyph, GlyphAdmin)
 admin.site.register(Style, StyleAdmin)
 admin.site.register(Name, NameAdmin)
 admin.site.register(Nomenclature, NomenclatureAdmin)
+admin.site.register(NameNomenclatureMembership, NameNomenclatureMembershipAdmin)
