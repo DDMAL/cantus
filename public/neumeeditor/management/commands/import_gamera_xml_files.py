11d265a0494f55f0c1251c528a17ef0a10c716f4
AFFogarty <andrew.f.fogarty@gmail.com>
2015-05-07 15:55:14 -0400

    Fixed: Only import a couple Gamera xml files

--- public/neumeeditor/management/commands/import_gamera_xml_files.py
+++ public/neumeeditor/management/commands/import_gamera_xml_files.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # Re-save every image, thereby regenerating all thumbnails
         #for i in range(15, 191):
-        for i in range(15, 20):
+        for i in range(15, 17):
             file_name = "390_{0}".format(str(i).zfill(3))
             import_gamera_file(BASE_DIR + "/data_dumps/gamera_xml/{0}".format(file_name))
             print "GameraXML file {0} imported.".format(file_name)
