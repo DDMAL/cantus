a5257c0f56a63fe9b384024af46c0f40ede259e3
AFFogarty <andrew.f.fogarty@gmail.com>
2015-05-12 14:02:09 -0400

    New: Spaces in gnome names are dots, not underscores

--- public/neumeeditor/models/fields/short_code_field.py
+++ public/neumeeditor/models/fields/short_code_field.py
@@ -4,15 +4,15 @@ from django.db import models
 
 class ShortCodeField(models.CharField):
     unacceptable_chars = "[^a-z0-9\._]"
-    duplicate_spaces = "\ +"
+    duplicate_spaces_and_dots = "[\ .]+"
     description = "A short string representing a glyph name"
 
     def pre_save(self, model_instance, add):
         """
         We want to filter-out the undesirable characters.
         """
-        # Turn spaces into single dashes
-        new_code = re.sub(self.duplicate_spaces, '_', model_instance.short_code.strip().lower())
+        # Turn spaces and dots into single dots
+        new_code = re.sub(self.duplicate_spaces_and_dots, '.', model_instance.short_code.strip().lower())
         # Filter out everything bad
         model_instance.short_code = re.sub(self.unacceptable_chars, '', new_code)
         return model_instance.short_code
