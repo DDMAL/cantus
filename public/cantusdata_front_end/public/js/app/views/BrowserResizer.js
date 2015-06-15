DDMAL/cantus

commit 07a779006dd0df59b368cdd814eec230352057a6

Fixed: manuscript title is styled with Bootstrap classes
Correct the Bootstrap class for the column and remove the styling which was compensating for it.

public/cantusdata_front_end/public/template-assembler/templates/underscore-templates/manuscript-template.html
@@ -2,16 +2,17 @@
     <div id="diva-column" class="col-xs-6"></div>
     <div class="col-xs-6">
         <div id="manuscript-title-container" class="row">
-            <div class="column-md-12">
+            <div class="col-md-12">
                 <h1 class="h3"><%= name %></h1>
-
-                <!-- Nav tabs -->
-                <ul class="nav nav-tabs nav-justified">
-                    <li class="active"><a href="#manuscript-folio-pane" data-toggle="tab">Folio</a></li>
-                    <li><a href="#manuscript-details-pane" data-toggle="tab">Manuscript Details</a></li>
-                    <li><a href="#manuscript-search-pane" data-toggle="tab">Search Manuscript</a></li>
-                </ul>
             </div>
+        </div>
+        <div class="row">
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs nav-justified">
+                <li class="active"><a href="#manuscript-folio-pane" data-toggle="tab">Folio</a></li>
+                <li><a href="#manuscript-details-pane" data-toggle="tab">Manuscript Details</a></li>
+                <li><a href="#manuscript-search-pane" data-toggle="tab">Search Manuscript</a></li>
+            </ul>
         </div>
         <div id="manuscript-data-container" class="row">
             <div class="tab-pane col-md-12 active">

public/cantusdata/static/css/styles.css
@@ -74,11 +74,6 @@ table.result-list th a {
     /*overflow: hidden;*/
 }
 
-#manuscript-title-container h1 {
-    margin-left: 15px;
-    margin-right: 15px;
-}
-
 #manuscript-data-container {
     overflow-y: scroll;
 }

commit cd475ad37d0677c9c96791a86b1803388b6e424c

Fixed: manuscript content panes don't overflow their container
Compensate for the nav tab height to fix a regression introduced by commit 07a7790.

public/cantusdata_front_end/public/js/app/views/BrowserResizer.js
@@ -66,7 +66,9 @@ define(["jquery", "backbone", "singletons/GlobalEventHandler"],
             setManuscriptContentContainerHeight: function()
             {
                 $('#manuscript-data-container').css("height",
-                        $("#content-container").height() - $("#manuscript-title-container").height());
+                        $("#content-container").height()
+                            - $("#manuscript-title-container").height()
+                            - $("#manuscript-nav-container").height());
             },
 
             /**

public/cantusdata_front_end/public/template-assembler/templates/underscore-templates/manuscript-template.html
@@ -6,7 +6,7 @@
                 <h1 class="h3"><%= name %></h1>
             </div>
         </div>
-        <div class="row">
+        <div id="manuscript-nav-container" class="row">
             <!-- Nav tabs -->
             <ul class="nav nav-tabs nav-justified">
                 <li class="active"><a href="#manuscript-folio-pane" data-toggle="tab">Folio</a></li>