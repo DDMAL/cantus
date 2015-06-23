95242f409c7b4f11c2978481e33245ac2a5db84d
William Bain <bain.william.a@gmail.com>
2015-06-23 18:07:59 -0400

    New: Input for Volpiano search is displayed as Volpiano
    Refs #201

--- public/cantusdata_front_end/public/js/app/views/SearchInputView.js
+++ public/cantusdata_front_end/public/js/app/views/SearchInputView.js
@@ -6,13 +6,15 @@ define(["marionette"],
             template: '#search-input-template',
 
             modelEvents: {
-                'change:field': 'updateFieldInput'
+                'change:field': 'updateFieldInput',
+                'change:query': 'updateQueryInput'
             },
 
             events: {
                 "change .search-field": "setField",
                 "change .search-input": "setQuery",
-                "input .search-input": "setQueryDebounced"
+                "input .search-input": "setQueryDebounced",
+                "keydown .search-input": "updateQueryInput"
             },
 
             ui: {
@@ -38,12 +40,49 @@ define(["marionette"],
 
             /**
              * Update the kind of input displayed when the field value changes. Currently, this
-             * just means re-rendering the view when the field goes to or from mode.
+             * entails re-rendering the view when the field goes to or from mode and changing
+             * the field class on the input element otherwise.
              */
             updateFieldInput: function ()
             {
-                if (this.model.get('field') === 'mode' || this.model.previous('field') === 'mode')
+                var field = this.model.get('field'),
+                    prev = this.model.previous('field');
+
+                if (this.requiresRerender(field) || this.requiresRerender(prev))
+                {
                     this.render();
+                }
+                else
+                {
+                    this.ui.searchInput.removeClass('field-' + prev).addClass('field-' + field);
+                }
+            },
+
+            /**
+             * Update the `search-text-entered` class on the query input when that input changes.
+             */
+            updateQueryInput: function ()
+            {
+                if (this.ui.searchInput.val())
+                {
+                    this.ui.searchInput.addClass('search-text-entered');
+                }
+                else
+                {
+                    this.ui.searchInput.removeClass('search-text-entered');
+                }
+            },
+
+            /**
+             * Return whether a change to or from the given search field should trigger a
+             * re-render
+             *
+             * @param fieldType
+             * @returns {boolean}
+             */
+            requiresRerender: function (fieldType)
+            {
+                return fieldType === 'volpiano';
             },
 
             onRender: function ()
