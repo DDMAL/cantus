a244539c7312144d399f1c41596943dcd4a11183
William Bain <bain.william.a@gmail.com>
2015-07-31 12:34:41 -0400

    Tests: Search notation result view
    Test the lifecycle of the search notation result view

---
+++ public/cantusdata/frontend/public/js/app/views/SearchNotationResultView.spec.js
@@ -0,0 +1,98 @@
+"use strict";
+
+describe('SearchNotationResultView', function ()
+{
+    var testSetup = require('test/setup');
+    var SearchNotationResultView = require('./SearchNotationResultView');
+    var SearchResultCollection = require('collections/SearchNotationResultCollection');
+
+    // Fixtures
+    var testParameters = {
+        field: "pnames",
+        fieldName: "Pitch",
+        query: "bcbccdcc",
+        manuscript: "cdn-hsmu-m2149l4"
+    };
+
+    var testResponse = {
+        status: 200,
+        contentType: 'application/json',
+        responseText: JSON.stringify({
+            numFound: 2,
+            results: [
+                {
+                    boxes: [
+                        {y: 4284, p: "004r", x: 2604, w: 125, h: 55},
+                        {y: 4452, p: "004r", x: 769, w: 244, h: 86}
+                    ],
+                    contour: ["u", "d", "u", "r", "u", "d", "r"],
+                    intervals: ["u2", "d2", "u9", "r", "u2", "d2", "r"],
+                    neumes: ["torculus", "punctum", "torculus", "punctum"],
+                    pnames: ["b", "c", "b", "c", "c", "d", "c", "c"],
+                    semitones: [1, -1, 13, 0, 2, -2, 0]
+                },
+                {
+                    boxes: [
+                        {y: 4123, p: "144v", x: 1564, w: 70, h: 112}
+                    ],
+                    contour: ["u", "d", "u", "r", "u", "d", "r"],
+                    intervals: ["u2", "d2", "u2", "r", "u2", "d2", "r"],
+                    neumes: ["torculus", "punctum", "punctum", "punctum", "punctum"],
+                    pnames: ["b", "c", "b", "c", "c", "d", "c", "c"],
+                    semitones: [1, -1, 1, 0, 2, -2, 0]
+                }
+            ]
+        })
+    };
+
+    beforeEach(function ()
+    {
+        jasmine.Ajax.install();
+
+        this.collection = new SearchResultCollection();
+
+        this.view = new SearchNotationResultView({
+            collection: this.collection
+        });
+
+        testSetup.showView(this.view);
+    });
+
+    afterEach(function ()
+    {
+        testSetup.clearView();
+        jasmine.Ajax.uninstall();
+    });
+
+    it('should hide the result table if there are no results', function ()
+    {
+        expect(this.view.ui.table).not.toBeVisible();
+    });
+
+    it('should show a spinner until the results load, then the result table', function ()
+    {
+        this.collection.updateParameters(testParameters);
+        this.collection.fetch();
+
+        expect(this.view.$('img[src="/static/img/loading.gif"]')).toBeInDOM();
+        expect(this.view.ui.table).not.toBeVisible();
+
+        jasmine.Ajax.requests.mostRecent().respondWith(testResponse);
+
+        expect(this.view.$('img[src="/static/img/loading.gif"]')).toHaveLength(0);
+        expect(this.view.ui.table).toBeVisible();
+        expect(this.view.$('tbody > tr')).toHaveLength(2);
+    });
+
+    it('should clear the results when the collection is reset', function ()
+    {
+        this.collection.updateParameters(testParameters);
+        this.collection.fetch();
+
+        jasmine.Ajax.requests.mostRecent().respondWith(testResponse);
+
+        expect(this.view.ui.table).toBeVisible();
+        this.collection.reset();
+        expect(this.view.ui.table).not.toBeVisible();
+    });
+});
