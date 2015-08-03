ee6c0d199696abe8a1f3b3b5f9b0ebc72463abad
William Bain <bain.william.a@gmail.com>
2015-08-03 15:58:23 -0400

    Refactor: Move search result item view into separate module

---
+++ public/cantusdata/frontend/public/js/app/views/SearchNotationResultItemView.js
@@ -0,0 +1,71 @@
+define([
+    "underscore",
+    "marionette"
+], function (_, Marionette)
+{
+    "use strict";
+
+    return Marionette.ItemView.extend({
+        template: '#search-notation-result-item-view',
+        tagName: 'tr',
+
+        events: {
+            'click .result-target': function (event)
+            {
+                event.preventDefault();
+                this.trigger('showResult');
+            }
+        },
+
+        templateHelpers: {
+            contourGraph: function (semitones)
+            {
+                var highest = 0, lowest = 0;
+                var height = 15, width = 50;
+
+                var relPitches = _.reduce(semitones, function (relPitches, semitone)
+                {
+                    relPitches.push(_.last(relPitches) + semitone);
+                    return relPitches;
+                }, [0]);
+
+                _.forEach(relPitches, function (pitch)
+                {
+                    if (pitch > highest)
+                        highest = pitch;
+                    else if (pitch < lowest)
+                        lowest = pitch;
+                });
+
+                var range = highest - lowest;
+                var stretch = relPitches.length === 1 ? 0.5 : relPitches.length - 1;
+
+                var points = _.map(relPitches, function (pitch, index)
+                {
+                    return {
+                        x: (index / stretch) * width,
+                        y: height - (range === 0 ? 0.5 : (pitch - lowest) / range) * height
+                    };
+                }, []);
+
+                var path = _.map(points, function (p, index)
+                {
+                    return (index === 0 ? 'M' : 'L') + p.x + ' ' + p.y;
+                }).join('');
+
+                var circles = _.map(points, function (p)
+                {
+                    return '<circle fill="black" r="2" cx="' + p.x + '" cy="' + p.y + '" />';
+                }).join('');
+
+                var padding = 2;
+                var viewBox = [-padding, -padding, width + padding * 2, height + padding * 2].join(' ');
+
+                return '<svg width="' + width + '" height="' + height + '" viewBox="' + viewBox + '">' +
+                    '<path stroke="black" fill="none" d="' + path + '"/>' +
+                    circles +
+                    '</svg>';
+            }
+        }
+    });
+});
