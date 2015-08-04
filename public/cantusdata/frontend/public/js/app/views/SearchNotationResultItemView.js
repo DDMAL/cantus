bcae7a5fbae459482843ea7915a5ed8483f193e5
William Bain <bain.william.a@gmail.com>
2015-08-04 14:14:08 -0400

    New: Show page snippets in OMR search results
    Instead of showing neume names, display the relevant sections of the page.
    
    Closes #225

--- public/cantusdata/frontend/public/js/app/views/SearchNotationResultItemView.js
+++ public/cantusdata/frontend/public/js/app/views/SearchNotationResultItemView.js
@@ -1,9 +1,13 @@
 define([
     "underscore",
-    "marionette"
-], function (_, Marionette)
+    "backbone",
+    "marionette",
+    "config/GlobalVars"
+], function (_, Backbone, Marionette, GlobalVars)
 {
     "use strict";
+
+    var manuscriptChannel = Backbone.Radio.channel('manuscript');
 
     return Marionette.ItemView.extend({
         template: '#search-notation-result-item-view',
@@ -17,55 +21,86 @@ define([
             }
         },
 
-        templateHelpers: {
-            contourGraph: function (semitones)
+        initialize: function ()
+        {
+            var manuscript = manuscriptChannel.request('model:manuscript');
+
+            this.siglumSlug = manuscript ? manuscript.get('siglum_slug') : null;
+        },
+
+        getThumbnails: function (boxes)
+        {
+            if (!this.siglumSlug)
             {
-                var highest = 0, lowest = 0;
-                var height = 15, width = 50;
-
-                var relPitches = _.reduce(semitones, function (relPitches, semitone)
-                {
-                    relPitches.push(_.last(relPitches) + semitone);
-                    return relPitches;
-                }, [0]);
-
-                _.forEach(relPitches, function (pitch)
-                {
-                    if (pitch > highest)
-                        highest = pitch;
-                    else if (pitch < lowest)
-                        lowest = pitch;
-                });
-
-                var range = highest - lowest;
-                var stretch = relPitches.length === 1 ? 0.5 : relPitches.length - 1;
-
-                var points = _.map(relPitches, function (pitch, index)
-                {
-                    return {
-                        x: (index / stretch) * width,
-                        y: height - (range === 0 ? 0.5 : (pitch - lowest) / range) * height
-                    };
-                }, []);
-
-                var path = _.map(points, function (p, index)
-                {
-                    return (index === 0 ? 'M' : 'L') + p.x + ' ' + p.y;
-                }).join('');
-
-                var circles = _.map(points, function (p)
-                {
-                    return '<circle fill="black" r="2" cx="' + p.x + '" cy="' + p.y + '" />';
-                }).join('');
-
-                var padding = 2;
-                var viewBox = [-padding, -padding, width + padding * 2, height + padding * 2].join(' ');
-
-                return '<svg width="' + width + '" height="' + height + '" viewBox="' + viewBox + '">' +
-                    '<path stroke="black" fill="none" d="' + path + '"/>' +
-                    circles +
-                    '</svg>';
+                return [];
             }
+
+            return _.map(boxes, function (box)
+            {
+                var siglum = this.siglumSlug;
+                var filename = GlobalVars.divaImageDirectory + siglum + '/' + siglum + '_' + box.p + '.jp2';
+                var bounds = [box.x, box.y, box.w, box.h].join(',');
+                var height = 30;
+
+                var iiifQuery = filename + '/' + bounds + '/,' + height + '/0/native.jpg';
+
+                return GlobalVars.iipImageServerUrl + '?IIIF=' + iiifQuery;
+            }, this);
+        },
+
+        templateHelpers: function ()
+        {
+            return {
+                contourGraph: function (semitones)
+                {
+                    var highest = 0, lowest = 0;
+                    var height = 15, width = 50;
+
+                    var relPitches = _.reduce(semitones, function (relPitches, semitone)
+                    {
+                        relPitches.push(_.last(relPitches) + semitone);
+                        return relPitches;
+                    }, [0]);
+
+                    _.forEach(relPitches, function (pitch)
+                    {
+                        if (pitch > highest)
+                            highest = pitch;
+                        else if (pitch < lowest)
+                            lowest = pitch;
+                    });
+
+                    var range = highest - lowest;
+                    var stretch = relPitches.length === 1 ? 0.5 : relPitches.length - 1;
+
+                    var points = _.map(relPitches, function (pitch, index)
+                    {
+                        return {
+                            x: (index / stretch) * width,
+                            y: height - (range === 0 ? 0.5 : (pitch - lowest) / range) * height
+                        };
+                    }, []);
+
+                    var path = _.map(points, function (p, index)
+                    {
+                        return (index === 0 ? 'M' : 'L') + p.x + ' ' + p.y;
+                    }).join('');
+
+                    var circles = _.map(points, function (p)
+                    {
+                        return '<circle fill="black" r="2" cx="' + p.x + '" cy="' + p.y + '" />';
+                    }).join('');
+
+                    var padding = 2;
+                    var viewBox = [-padding, -padding, width + padding * 2, height + padding * 2].join(' ');
+
+                    return '<svg width="' + width + '" height="' + height + '" viewBox="' + viewBox + '">' +
+                        '<path stroke="black" fill="none" d="' + path + '"/>' +
+                        circles +
+                        '</svg>';
+                },
+                thumbnails: _.bind(this.getThumbnails, this)
+            };
         }
     });
 });
