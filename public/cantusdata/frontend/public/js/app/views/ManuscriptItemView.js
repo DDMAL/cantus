80457a2d51a20ea0b80309c9efeb0ed5ca2a57a1
William Bain <bain.william.a@gmail.com>
2015-08-12 16:42:31 -0400

    New: Link to CANTUS website from Manuscripts page

--- public/cantusdata/frontend/public/js/app/views/ManuscriptItemView.js
+++ public/cantusdata/frontend/public/js/app/views/ManuscriptItemView.js
@@ -4,6 +4,17 @@ define(["marionette"], function (Marionette)
 
     return Marionette.ItemView.extend({
         template: '#manuscript-item-template',
-        tagName: 'li'
+        tagName: 'li',
+
+        templateHelpers: {
+            // Map to the Cantus URLs for the various manuscripts
+            // Let this be a warning about the danger of non-meaningful URLs, I guess
+            cantusUrls: {
+                'cdn-hsmu-m2149l4': 'http://cantus.uwaterloo.ca/source/123723',
+                'ch-sgs-390': 'http://cantus.uwaterloo.ca/source/123717',
+                'ch-sgs-391': 'http://cantus.uwaterloo.ca/source/123718',
+                'nl-uu-406': 'http://cantus.uwaterloo.ca/source/123641'
+            }
+        }
     });
 });
