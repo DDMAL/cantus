74e0e96656d7ce5e6d01a2e45cba05292f7a71df
William Bain <bain.william.a@gmail.com>
2016-03-04 11:35:07 -0500

    Fixed: Remove unused views

--- public/cantusdata/frontend/public/js/app/views/item_views/GlyphTypeView.js
+++
@@ -1,9 +0,0 @@
-import Marionette from 'marionette';
-
-/**
- * View representing a Glyph Type with count.
- */
-export default Marionette.ItemView.extend({
-    template: "#single-glyph-type-template",
-    tagName: "li"
-});
