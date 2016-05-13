58d55186d77e4ea371a214535ec1767b071b5e65
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2016-05-13 11:36:25 -0400

    Refactor: Use single solr synchronizer session to delete all objects

--- public/cantusdata/management/commands/import_all_data.py
+++ public/cantusdata/management/commands/import_all_data.py
@@ -4,22 +4,24 @@ from cantusdata.models.chant import Chant
 from cantusdata.models.folio import Folio
 from cantusdata.models.concordance import Concordance
 from cantusdata.models.manuscript import Manuscript
+from cantusdata.signals.solr_sync import solr_synchronizer
 
 
 class Command(BaseCommand):
     args = ""
 
     def handle(self, *args, **kwargs):
-        # Delete all of the data in the db
-        self.stdout.write("Deleting all old chant data...")
-        Chant.objects.all().delete()
-        self.stdout.write("Old chants deleted.\nDeleting old folio data.")
-        Folio.objects.all().delete()
-        self.stdout.write("Old folios deleted.\nDeleting old manuscript data.")
-        Manuscript.objects.all().delete()
-        self.stdout.write("Old manuscripts deleted.\nDeleting old concordance data.")
-        Concordance.objects.all().delete()
-        self.stdout.write("Old concordances deleted.")
+        with solr_synchronizer.get_session():
+            # Delete all of the data in the db
+            self.stdout.write("Deleting all old chant data...")
+            Chant.objects.all().delete()
+            self.stdout.write("Old chants deleted.\nDeleting old folio data.")
+            Folio.objects.all().delete()
+            self.stdout.write("Old folios deleted.\nDeleting old manuscript data.")
+            Manuscript.objects.all().delete()
+            self.stdout.write("Old manuscripts deleted.\nDeleting old concordance data.")
+            Concordance.objects.all().delete()
+            self.stdout.write("Old concordances deleted.")
 
         # Load up the new data...
         call_command('import_manuscript_data', 'sources-export.csv', **kwargs)
