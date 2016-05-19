f36bb869d934e92aefaafbb06275c5b51eb4e29a
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2016-05-19 16:47:45 -0400

    Refactor: removed music21 dependency
    Closes #259

--- public/cantusdata/helpers/mei_conversion/pitch_utils.py
+++ public/cantusdata/helpers/mei_conversion/pitch_utils.py
@@ -1,5 +1,12 @@
-from music21.pitch import STEPREF
-from music21.interval import convertSemitoneToSpecifierGeneric
+STEPREF = {
+    'C': 0,
+    'D': 2,  # 2
+    'E': 4,
+    'F': 5,
+    'G': 7,
+    'A': 9,  # 9
+    'B': 11
+}
 
 
 def getIntervals(semitones, pnames):
@@ -99,3 +106,113 @@ def convertStepToPs(step, oct):
     step = step.strip().upper()
     ps = float(((oct + 1) * 12) + STEPREF[step])
     return ps
+
+
+def convertSemitoneToSpecifierGeneric(count):
+    """
+    --- From music21 ---
+
+    Given a number of semitones, return a default diatonic specifier.
+
+    >>> interval.convertSemitoneToSpecifierGeneric(0)
+    ('P', 1)
+    >>> interval.convertSemitoneToSpecifierGeneric(-2)
+    ('M', -2)
+    >>> interval.convertSemitoneToSpecifierGeneric(1)
+    ('m', 2)
+    >>> interval.convertSemitoneToSpecifierGeneric(7)
+    ('P', 5)
+    >>> interval.convertSemitoneToSpecifierGeneric(11)
+    ('M', 7)
+    >>> interval.convertSemitoneToSpecifierGeneric(12)
+    ('P', 8)
+    >>> interval.convertSemitoneToSpecifierGeneric(13)
+    ('m', 9)
+    >>> interval.convertSemitoneToSpecifierGeneric(-15)
+    ('m', -10)
+    >>> interval.convertSemitoneToSpecifierGeneric(24)
+    ('P', 15)
+    """
+    # strip off microtone
+    return convertSemitoneToSpecifierGenericMicrotone(count)[:2]
+
+
+def convertSemitoneToSpecifierGenericMicrotone(count):
+    """
+    --- From music21 ---
+
+    Given a number of semitones, return a default diatonic specifier and cent offset.
+    DEPRECATED if it can be moved..
+    >>> interval.convertSemitoneToSpecifierGenericMicrotone(2.5)
+    ('M', 2, 50.0)
+    >>> interval.convertSemitoneToSpecifierGenericMicrotone(2.25)
+    ('M', 2, 25.0)
+    >>> interval.convertSemitoneToSpecifierGenericMicrotone(1.0)
+    ('m', 2, 0.0)
+    >>> interval.convertSemitoneToSpecifierGenericMicrotone(1.75)
+    ('M', 2, -25.0)
+    >>> interval.convertSemitoneToSpecifierGenericMicrotone(1.9)
+    ('M', 2, -10.0...)
+    >>> interval.convertSemitoneToSpecifierGenericMicrotone(0.25)
+    ('P', 1, 25.0)
+    >>> interval.convertSemitoneToSpecifierGenericMicrotone(12.25)
+    ('P', 8, 25.0)
+    >>> interval.convertSemitoneToSpecifierGenericMicrotone(24.25)
+    ('P', 15, 25.0)
+    >>> interval.convertSemitoneToSpecifierGenericMicrotone(23.75)
+    ('P', 15, -25.0)
+    """
+    if count < 0:
+        dirScale = -1
+    else:
+        dirScale = 1
+
+    count, micro = divmod(count, 1)
+    # convert micro to cents
+    cents = micro * 100.0
+    if cents > 50:
+        cents -= 100
+        count += 1
+
+    count = int(count)
+    size = abs(count) % 12
+    octave = abs(count) // 12  # let floor to int
+
+    if size == 0:
+        spec = 'P'
+        generic = 1
+    elif size == 1:
+        spec = 'm'
+        generic = 2
+    elif size == 2:
+        spec = 'M'
+        generic = 2
+    elif size == 3:
+        spec = 'm'
+        generic = 3
+    elif size == 4:
+        spec = 'M'
+        generic = 3
+    elif size == 5:
+        spec = 'P'
+        generic = 4
+    elif size == 6:
+        spec = 'd'
+        generic = 5
+    elif size == 7:
+        spec = 'P'
+        generic = 5
+    elif size == 8:
+        spec = 'm'
+        generic = 6
+    elif size == 9:
+        spec = 'M'
+        generic = 6
+    elif size == 10:
+        spec = 'm'
+        generic = 7
+    elif size == 11:
+        spec = 'M'
+        generic = 7
+
+    return spec, (generic+(octave*7)) * dirScale, cents
