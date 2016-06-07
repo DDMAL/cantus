5928ad2995e12dc17bcae4c3400984d5f295a2a6
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2016-06-07 14:26:05 -0400

    Travis: Hopefully this works

--- etc/ci/prepare-server.sh
+++ etc/ci/prepare-server.sh
@@ -11,26 +11,8 @@ pwd
 
 solr -h
 echo "Starting Solr"
-#solr start -p 8080
-#echo "Getting Solr Status"
-#solr status
-
-(
-    cd /home/travis
-    ls
-    cd solr
-    ls
-    cd solr-6.0.1
-    ls
-    cd server
-    ls -l
-    cd solr
-    echo "In Solr"
-    ls
-)
-
 solr start -p 8080
-
+echo "Getting Solr Status"
 solr status
 
 source app_env/bin/activate
