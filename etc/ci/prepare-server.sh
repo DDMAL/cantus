bc3227da7f2f6d8cdaf36ef1ea1b7fc72a7af2eb
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2016-06-07 10:48:39 -0400

    Travis: Debug info

--- etc/ci/prepare-server.sh
+++ etc/ci/prepare-server.sh
@@ -6,19 +6,15 @@ set -e
 
 cd ./public
 
-(
-    cd solr
-
-    # The `which mvn` seems to be a necessary workaround for some kind of path reset problem
-    # FIXME: The output here probably shouldn't be swallowed
-    sudo `which mvn` jetty:run-war > /dev/null 2>&1 &
-)
+solr start -p 8080
 
 source app_env/bin/activate
 
 cp ./cantusdata/settings-example.py ./cantusdata/settings.py
 
-./manage.py wait_until_solr_ready
+solr status
+
+./manage.py wait_until_solr_ready --timeout=20000
 
 ./manage.py makemigrations
 ./manage.py migrate
