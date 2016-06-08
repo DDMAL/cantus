d3c3fdb21a5b8d97532ecf7e80f897d6081af118
Jérôme Parent-Lévesque <jerome-pl2@hotmail.ca>
2016-06-08 09:07:58 -0400

    Travis: Assume Solr is ready, can we create core?

--- etc/ci/prepare-server.sh
+++ etc/ci/prepare-server.sh
@@ -14,7 +14,7 @@ source app_env/bin/activate
 
 cp ./cantusdata/settings-example.py ./cantusdata/settings.py
 
-./manage.py wait_until_solr_ready --timeout=90
+# ./manage.py wait_until_solr_ready --timeout=90
 
 ./manage.py makemigrations
 ./manage.py migrate
