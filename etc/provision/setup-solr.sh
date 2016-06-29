9f59df203afb0baa53992776529df17ef9974792
jeromepl <jerome-pl2@hotmail.ca>
2016-06-29 15:53:36 -0400

    Fixed: Travis is now working

--- etc/provision/setup-solr.sh
+++ etc/provision/setup-solr.sh
@@ -70,7 +70,6 @@ fi
 if [ ! -d /var/db/solr ]; then
     sudo mkdir -p /var/db/solr
     sudo mkdir -p /var/db/solr/cantusdata-solr
-    ps aux | grep "solr"
-    #sudo chmod a+w /var/db/solr -R
+    sudo chmod a+w /var/db/solr -R
     sudo chgrp www-data /var/db/solr -R
 fi
