850c4fa5bf09b71b97c611bac75b0c04f9390939
jeromepl <jerome-pl2@hotmail.ca>
2016-06-29 12:55:52 -0400

    Travis: Debug Solr unable to create index

--- etc/ci/prepare-server.sh
+++ etc/ci/prepare-server.sh
@@ -10,6 +10,8 @@ solr start -p 8080
 
 solr status
 
+tail -n 50 /home/travis/solr/solr-6.1.0/server/logs/solr.log
+
 source app_env/bin/activate
 
 cp ./cantusdata/settings-example.py ./cantusdata/settings.py
