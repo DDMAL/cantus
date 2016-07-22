2044be53fdfeef72ad54ac8751cc1f242370bdbc
jeromepl <jerome-pl2@hotmail.ca>
2016-07-22 12:43:19 -0400

    New: Better message saying no chants start on current folio
    Since chants can start on the previous page, this message is
    better than saying that there are no chants on the page.
    Thanks to Debra Lacoste for this formulation.

--- public/cantusdata/frontend/public/js/app/manuscript-detail/folio/ChantCompositeView.js
+++ public/cantusdata/frontend/public/js/app/manuscript-detail/folio/ChantCompositeView.js
@@ -156,7 +156,7 @@ export default Marionette.CompositeView.extend({
         if (this.collection.length === 0)
         {
             // No chants
-            this.ui.errorMessages.html("This folio does not contain musical data.");
+            this.ui.errorMessages.html("No chants begin on this page or folio side.");
         }
         else
         {
