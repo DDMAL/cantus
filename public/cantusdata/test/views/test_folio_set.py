9df43556bc391f49bf79966234d3cdf8851f16c8
jeromepl <jerome-pl2@hotmail.ca>
2016-07-22 09:16:17 -0400

    Fixed: Tests using MEI converter

--- public/cantusdata/test/views/test_folio_set.py
+++ public/cantusdata/test/views/test_folio_set.py
@@ -15,16 +15,16 @@ class ManuscriptFolioSetViewTestCase(APITransactionTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Test that it's the response we're expecting
         expected_string = '[{"manuscript_id": 3, "number": "123",' \
-                          ' "item_id": "1", "type": "cantusdata_folio"}]'
+                          ' "item_id": "1", "type": "cantusdata_folio", "image_uri": "http://test.com"}]'
         # We want to remove the version id and unique id because they're always
         # different.
         self.assertJSONEqual(response.content, expected_string)
 
     def test_get_specific_folio(self):
-        response = self.client.get("/folio-set/manuscript/3/123/")
+        response = self.client.get("/folio-set/manuscript/3/http%3A%2F%2Ftest.com/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         expected_string = '{"manuscript_id": 3, "number": "123",' \
-                          ' "item_id": "1", "type": "cantusdata_folio"}'
+                          ' "item_id": "1", "type": "cantusdata_folio", "image_uri": "http://test.com"}'
         # Empty response is just square brackets
         self.assertJSONEqual(response.content, expected_string)
 
