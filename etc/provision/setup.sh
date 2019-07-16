745d209cbf8c9ba933a59068cdade26fc461141a
Nestor Napoles <napulen@gmail.com>
2019-07-16 16:57:42 -0400

    Writing a new version of the provisioning script for Vagrant

--- etc/provision/setup.sh
+++ etc/provision/setup.sh
@@ -14,9 +14,30 @@ else
 fi
 
 echo "Updating package listings"
-sudo apt-get -qq update
+sudo apt-get update
 
-"$ROOT/etc/provision/setup-django.sh" "$ROOT"
-"$ROOT/etc/provision/setup-solr.sh" "$ROOT"
-"$ROOT/etc/provision/setup-pymei.sh" "$ROOT"
+# "$ROOT/etc/provision/setup-django.sh" "$ROOT"
+# "$ROOT/etc/provision/setup-solr.sh" "$ROOT"
+# "$ROOT/etc/provision/setup-pymei.sh" "$ROOT"
 
+# Additional packages
+
+sudo apt install -y build-essential libxml2-dev libxslt-dev libxslt1-dev zlib1g-dev libjpeg8-dev libpq-dev python-dev
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
+
+# Create the cantus user 
+sudo groupadd --system webapps
+sudo useradd --system --gid webapps --shell /bin/bash --home /srv/webapps/cantus cantus
+sudo echo "cantus ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Setup postgres
+sudo apt-get install -y postgresql postgresql-contrib
+sudo -u postgres psql -c "create user cantus_admin with encrypted password '${CANTUS_ADMIN_PASSWORD}';"
+sudo -u postgres psql -c "alter user cantus_admin with SUPERUSER;"
+sudo -u postgres psql -c "create database cantus_db with owner cantus_admin;"
+
+# Copy the repository
+sudo mkdir -p /srv/webapps/cantus/
+sudo cp -R /vagrant/* /srv/webapps/cantus/
+sudo chown -R cantus /srv/webapps/cantus/
