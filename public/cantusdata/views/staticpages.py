6e84032a30468d8fe7cbc5f4efbdd1802e1233ef
Nestor Napoles <napulen@gmail.com>
2019-08-09 16:59:39 -0400

    Adding a parser for the title of the static page

--- public/cantusdata/views/staticpages.py
+++ public/cantusdata/views/staticpages.py
@@ -2,18 +2,20 @@ from django.shortcuts import render
 from django.conf import settings
 import markdown
 import os.path
+import re
 import codecs
 
 def homepage(request):
         markdown_file = '../cantus-staticpages/content/home.md'
-        content_html = markdown.markdown(
-                codecs.open(os.path.join(settings.BASE_DIR, markdown_file),
-                encoding='utf-8').read())
-        return render(request, 'flatpages/default.html', {'content': content_html, 'title': 'TestTitle'})
+        content = codecs.open(os.path.join(settings.BASE_DIR, markdown_file), 
+            encoding='utf-8').read()
+        content_html = markdown.markdown(content)
+        return render(request, 'flatpages/default.html', {'content': content_html, 'title': ''})
 
 def general(request, static_page):
         markdown_file = '../cantus-staticpages/content/{}.md'.format(static_page)
-        content_html = markdown.markdown(
-                codecs.open(os.path.join(settings.BASE_DIR, markdown_file),
-                encoding='utf-8').read())
-        return render(request, 'flatpages/default.html', {'content': content_html, 'title': 'TestTitle'})
+        content = codecs.open(os.path.join(settings.BASE_DIR, markdown_file), 
+            encoding='utf-8').read()
+        title = re.match(r"^#([A-Za-z0-9 _-]+)", content)
+        content_html = markdown.markdown(content)
+        return render(request, 'flatpages/default.html', {'content': content_html, 'title': title})
