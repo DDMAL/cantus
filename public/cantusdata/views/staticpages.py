d41ed3dff8475319262582db25a17be2bcd27608
Nestor Napoles <napulen@gmail.com>
2019-08-09 16:03:16 -0400

    New: Adding the staticpages view

---
+++ public/cantusdata/views/staticpages.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.conf import settings
+import markdown
+import os.path
+import codecs
+
+def homepage(request):
+        markdown_file = '../cantus-staticpages/content/home.md'
+        content_html = markdown.markdown(
+                codecs.open(os.path.join(settings.BASE_DIR, markdown_file),
+                encoding='utf-8').read())
+        return render(request, 'polls/staticpage.html', {'content': content_html})
+
+def staticpage(request, static_page):
+        markdown_file = '../cantus-staticpages/content/{}.md'.format(static_page)
+        content_html = markdown.markdown(
+                codecs.open(os.path.join(settings.BASE_DIR, markdown_file),
+                encoding='utf-8').read())
+        return render(request, 'polls/staticpage.html', {'content': content_html})
