88c1a85b21d7a774d71842d991f2263d3d51347b
Nestor Napoles <napulen@gmail.com>
2019-08-12 12:48:00 -0400

    Doc: Writing docstrings for the views of the staticpages

--- public/cantusdata/views/staticpages.py
+++ public/cantusdata/views/staticpages.py
@@ -6,19 +6,35 @@ import re
 import codecs
 
 def homepage(request):
+        """The view of the homepage, generated from a Markdown file.
+
+        The content is taken from a Markdown file, which is always located at `home.md` of the `cantus-staticpages` repository.
+        """
         markdown_file = '../cantus-staticpages/content/home.md'
-        content = codecs.open(os.path.join(settings.BASE_DIR, markdown_file), 
-            encoding='utf-8').read()
-        content_html = markdown.markdown(content)
-        return render(request, 'flatpages/default.html', {'content': content_html, 'title': ''})
+        markdown_fullpath = os.path.join(settings.BASE_DIR, markdown_file)
+        content = codecs.open(markdown_fullpath, encoding='utf-8').read()
+        content_as_html = markdown.markdown(content)
+        context = {'content': content_as_html}
+        return render(request, 'staticpages/homepage.html', context)
 
 def general(request, static_page):
-        markdown_file = '../cantus-staticpages/content/{}.md'.format(static_page)
-        content = codecs.open(
-            os.path.join(settings.BASE_DIR, markdown_file), 
-            encoding='utf-8').readlines()
-        title_line = content[0]
-        the_rest = '\n'.join(content[1:])
+        """The view of a generic static page generated from a Markdown file.
+
+        The first line of the markdown is considered the title.
+        The rest of the markdown is considered the content.
+
+        Parameters
+        ----------
+        static_page : str
+            Name of the markdown file where the content is to be found, which is extracted from the url of the staticpage in `urls.py`
+        """
+        formatstr = '../cantus-staticpages/content/{}.md'
+        markdown_file = formatstr.format(static_page)
+        mrkdwn_fullpath = os.path.join(settings.BASE_DIR, markdown_file)
+        titlecont = codecs.open(mrkdwn_fullpath, encoding='utf-8').readlines()
+        title_line = titlecont[0]
+        content = '\n'.join(titlecont[1:])
         title = re.match(r"^#([A-Za-z0-9 _-]+)$", title_line).group(1)
-        content_html = markdown.markdown(the_rest)
-        return render(request, 'flatpages/default.html', {'content': content_html, 'title': title})
+        content_as_html = markdown.markdown(content)
+        context = {'title': title, 'content': content_as_html}
+        return render(request, 'staticpages/general.html', context)
