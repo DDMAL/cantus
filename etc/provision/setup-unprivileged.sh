1d858056254eddc60e5333686fbd103af21e48ee
Nestor Napoles Lopez <napulen@gmail.com>
2020-02-17 21:17:10 -0500

    Removing requirements-dev.txt from the provisioning script

--- etc/provision/setup-unprivileged.sh
+++ etc/provision/setup-unprivileged.sh
@@ -5,7 +5,6 @@ cd public
 virtualenv app_env
 source app_env/bin/activate
 pip install -r requirements.txt
-pip install -r requirements-dev.txt
 
 python manage.py makemigrations
 python manage.py migrate
