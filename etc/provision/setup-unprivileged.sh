4415211848755cd4d695a54ab2dc9cf2ba100d10
Nestor Napoles Lopez <napulen@gmail.com>
2020-03-24 09:37:44 -0400

    Removing all remaining python2 files

--- etc/provision/setup-unprivileged.sh
+++ etc/provision/setup-unprivileged.sh
@@ -2,10 +2,9 @@
 
 echo "===== Setting up django ====="
 cd public
-virtualenv app_env
-python3.5 -m venv app_env3
-source app_env3/bin/activate
-pip install -r requirements3.txt
+python3.5 -m venv app_env
+source app_env/bin/activate
+pip install -r requirements.txt
 
 python manage.py makemigrations
 python manage.py migrate
