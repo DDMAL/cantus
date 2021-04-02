030c40ea3faf07e69a0df1f6b4d737639c2c4e4c
Nestor Napoles Lopez <napulen@gmail.com>
2021-04-02 08:48:43 -0400

    Removing dead commented code on the manuscript model.

--- public/cantusdata/models/manuscript.py
+++ public/cantusdata/models/manuscript.py
@@ -19,9 +19,6 @@ class Manuscript(models.Model):
 
     name = models.CharField(max_length=255, blank=True, null=True)
     siglum = models.CharField(max_length=255, blank=True, null=True)
-    # siglum_slug = models.SlugField(max_length=255, unique=True, blank=True,
-    #                                null=True)
-    # reduced max_length, should be safe
     date = models.CharField(max_length=50, blank=True, null=True)
     provenance = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
