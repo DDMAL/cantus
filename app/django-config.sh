52932a59f1e445f02b7381fe796b3231913744da
Eli Waksbaum <eli@waksbaum.com>
2022-05-11 14:20:02 -0400

    access site through nginx, serving django on 8001

--- app/django-config.sh
+++ app/django-config.sh
@@ -3,4 +3,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-python /code/public/manage.py runserver 0:8000
+python /code/public/manage.py runserver 0:8001
