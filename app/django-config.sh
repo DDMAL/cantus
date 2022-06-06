46886bee04c6c726ac95f1e1a550d8751f52bb5e
Eli Waksbaum <eli@waksbaum.com>
2022-06-06 16:21:13 -0400

    gunicorn on production, django on dev

--- app/django-config.sh
+++ app/django-config.sh
@@ -3,4 +3,8 @@
 python manage.py makemigrations
 python manage.py migrate &
 
-gunicorn -b 0:8001 cantusdata.wsgi --timeout 600 --workers 4
+if [[ $APP_PORT = 80 ]]; then
+    gunicorn -b 0:8001 cantusdata.wsgi --timeout 600 --workers 4
+else
+    python manage.py runserver 0:8001
+fi
