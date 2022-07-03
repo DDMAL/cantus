41b19ec91f6fcdcce119c534105ef5385c54ed2a
dchiller <dylanh@shortesttrack.com>
2022-07-03 16:59:02 -0400

    Blackify containerization of celery worker

--- app/public/cantusdata/celery.py
+++ app/public/cantusdata/celery.py
@@ -4,9 +4,10 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cantusdata.settings")
 app = Celery("cantusdata")
 import cantusdata.settings as settings
-#app.config_from_object("django.conf:settings", namespace="CELERY")
+
 app.config_from_object(settings, namespace="CELERY")
 app.autodiscover_tasks()
+
 
 @app.task(bind=True)
 def debug_task(self):
