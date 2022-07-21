3756a06b71e955e8f469cc30deac54efa72012d3
Dylan Hillerbrand <dhillerbrand@gmail.com>
2022-07-21 11:05:57 -0400

    Lint PR #532

--- app/public/cantusdata/helpers/postprocess_iiif.py
+++ app/public/cantusdata/helpers/postprocess_iiif.py
@@ -8,16 +8,14 @@ def d_ka_aug_lx(datadict):
     The width and height attributes provided per folio distort the logic of the
     viewer. We only get sections of the folio instead of the entire folio.
     """
-    canvases = datadict['sequences'][0]['canvases']
+    canvases = datadict["sequences"][0]["canvases"]
     for canvas in canvases:
-        images = canvas['images']
+        images = canvas["images"]
         for image in images:
-            resource = image['resource']
-            resource.pop('height')
-            resource.pop('width')
+            resource = image["resource"]
+            resource.pop("height")
+            resource.pop("width")
     return datadict
 
 
-iiif_fn = {
-    "https://digital.blb-karlsruhe.de/i3f/v20/1253122/manifest": d_ka_aug_lx
-}
+iiif_fn = {"https://digital.blb-karlsruhe.de/i3f/v20/1253122/manifest": d_ka_aug_lx}
