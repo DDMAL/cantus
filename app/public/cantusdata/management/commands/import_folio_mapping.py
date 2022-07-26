5b14f0754cdbdf0f86f4708c3bf9fe8d5b33f1fb
Dylan Hillerbrand <dhillerbrand@gmail.com>
2022-07-26 10:20:22 -0400

    Move atomic decorator for async map folios task

--- app/public/cantusdata/management/commands/import_folio_mapping.py
+++ app/public/cantusdata/management/commands/import_folio_mapping.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from cantusdata.models.folio import Folio
 from cantusdata.models.manuscript import Manuscript
 from django.core.management import call_command
+from django.db import transaction
 import csv
 
 
@@ -51,6 +52,7 @@ class Command(BaseCommand):
             help="Optional argument used when called from the django admin interface. Passes asynchronous task.",
         )
 
+    @transaction.atomic
     def handle(self, *args, **options):
 
         manuscript_ids = options["manuscript_ids"]
