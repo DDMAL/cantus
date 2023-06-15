5deb712343f737aab5c5944bea1d68501c32545d
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-06-15 17:11:19 -0400

    Use runserver_plus for development

--- app/django-config.sh
+++ app/django-config.sh
@@ -7,5 +7,5 @@ python manage.py clear_session_data
 if [[ $APP_PORT = 80 ]]; then
     gunicorn -b 0:8001 cantusdata.wsgi --timeout 600 --workers 4
 else
-    python manage.py runserver 0:8001
+    python manage.py runserver_plus 0:8001
 fi
