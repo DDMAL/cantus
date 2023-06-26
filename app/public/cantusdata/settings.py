e661db75babd3b560bd8a5fb7fd06608e3b1c800
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-06-26 15:43:44 -0400

    Upgrade celery, Markdown, requests, coverage, django-celery-results, Werkzeug

--- app/public/cantusdata/settings.py
+++ app/public/cantusdata/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "cantusdata.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
