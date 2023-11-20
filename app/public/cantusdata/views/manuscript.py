dd9c32c9189e8c65f4c76de5a64cc4343153fce0
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-11-20 14:28:18 -0500

    Redirect to ManuscriptListView when manuscript is not found

--- app/public/cantusdata/views/manuscript.py
+++ app/public/cantusdata/views/manuscript.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
-from django.views import generic
 from cantusdata.models.manuscript import Manuscript
 from cantusdata.serializers.manuscript import (
     ManuscriptSerializer,
     ManuscriptListSerializer,
 )
 from cantusdata.renderers import templated_view_renderers
+from django.http import Http404, HttpResponseRedirect
+from rest_framework.response import Response
+from django.urls import reverse
 
 
 class ManuscriptList(generics.ListAPIView):
@@ -22,3 +24,19 @@ class ManuscriptDetail(generics.RetrieveAPIView):
     serializer_class = ManuscriptSerializer
     template_name = "require.html"
     renderer_classes = templated_view_renderers
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Overrides the default retrieval method
+        to return a redirect to the manuscript list
+        page if the manuscript is not found. Implemented
+        to prevent 404 errors from links to specific manuscripts
+        made before manuscript ids were stable.
+        """
+
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data)
+        except Http404:
+            return HttpResponseRedirect(reverse("manuscript-list"))
