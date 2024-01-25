d21f67a2c5ae1b68c28e62a692469b5fd00315bd
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-01-25 15:12:03 -0500

    Format differentiae_database migrations

--- app/public/cantusdata/migrations/0003_chant_differentiae_database.py
+++ app/public/cantusdata/migrations/0003_chant_differentiae_database.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('cantusdata', '0002_remove_chant_concordances_delete_concordance'),
+        ("cantusdata", "0002_remove_chant_concordances_delete_concordance"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='chant',
-            name='differentiae_database',
+            model_name="chant",
+            name="differentiae_database",
             field=models.CharField(blank=True, max_length=255),
         ),
     ]
