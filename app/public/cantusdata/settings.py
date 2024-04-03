7aea3a56a2a16693791681927cd9518af23b25e3
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-04-03 08:44:39 -0400

    Modify docker volumes
    - Add solr volume containing solr index
    - Change path of volume containing django-managed
    static files

--- app/public/cantusdata/settings.py
+++ app/public/cantusdata/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static/django/"
 MEDIA_URL = "/media/"
 MEDIA_URL_NEUMEEDITOR = "/neumeeditor/media/"
 
