96df819ff2ec5bb9798034cd8248c9ed7dcd0132
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-04-16 08:39:47 -0400

    Add explicit export of models

--- app/public/cantusdata/models/__init__.py
+++ app/public/cantusdata/models/__init__.py
@@ -2,3 +2,5 @@ from cantusdata.models.chant import Chant
 from cantusdata.models.manuscript import Manuscript
 from cantusdata.models.folio import Folio
 from cantusdata.models.neume_exemplar import NeumeExemplar
+
+__all__ = ["Chant", "Manuscript", "Folio", "NeumeExemplar"]
