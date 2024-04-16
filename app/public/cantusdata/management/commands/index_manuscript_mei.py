6ec0671341e254891cbbc3cae87b4a59993ca4a7
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-04-16 08:40:21 -0400

    Add index_manuscript_mei command and tests

---
+++ app/public/cantusdata/management/commands/index_manuscript_mei.py
@@ -0,0 +1,110 @@
+from typing import Any, Dict
+from os import path, listdir
+
+from django.core.management.base import BaseCommand, CommandParser
+from django.conf import settings
+from cantusdata.helpers.mei_processing.mei_tokenizer import MEITokenizer
+from cantusdata.models.folio import Folio
+
+from solr.core import SolrConnection  # type: ignore
+
+MEI4_DIR = path.join("code", "production-mei-files")
+
+
+class Command(BaseCommand):
+    help = (
+        "This command indexes the contents of MEI files in Solr, using"
+        "the MEITokenizer class to extract n-grams from the MEI files."
+        "Files must be named in the format [some string]_[folio number].mei."
+    )
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "manuscript_id",
+            type=int,
+            nargs=1,
+            help=(
+                "The ID of the manuscript whose MEI data should be indexed."
+                "Must have MEI files in a subdirectory of the --mei-dir argument"
+                "named with this ID."
+            ),
+        )
+        parser.add_argument(
+            "--mei-dir",
+            type=str,
+            default=MEI4_DIR,
+            help=(
+                "The directory containing the MEI files to be indexed."
+                "Defaults to 'code/production-mei-files'."
+            ),
+        )
+        parser.add_argument(
+            "--min-ngram",
+            type=int,
+            default=1,
+            help="The minimum n-gram length to index from the MEI files.",
+        )
+        parser.add_argument(
+            "--max-ngram",
+            type=int,
+            default=5,
+            help="The maximum n-gram length to index from the MEI files.",
+        )
+        parser.add_argument(
+            "--flush-index",
+            action="store_true",
+            help=(
+                "If this flag is set, the command will delete all existing OMR"
+                "documents for the specified manuscript before indexing the new data."
+            ),
+        )
+        parser.add_argument(
+            "--test-core",
+            action="store_true",
+            help=(
+                "Use the test core for indexing. Used when running unittests on this"
+                "command. Defaults to False."
+            ),
+        )
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        if options["test_core"]:
+            solr_conn = SolrConnection(settings.SOLR_TEST_SERVER)
+        else:
+            solr_conn = SolrConnection(settings.SOLR_SERVER)
+        manuscript_id = options["manuscript_id"][0]
+        if options.get("flush_index"):
+            self.flush_manuscript_ngrams_from_index(solr_conn, manuscript_id)
+        else:
+            folio_map: Dict[str, str] = dict(
+                Folio.objects.filter(manuscript_id=manuscript_id).values_list(
+                    "number", "image_uri"
+                )
+            )
+            manuscript_mei_path = path.join(options["mei_dir"], str(manuscript_id))
+            manuscript_mei_files = [
+                f for f in listdir(manuscript_mei_path) if f.endswith(".mei")
+            ]
+            for mei_file in manuscript_mei_files:
+                folio_number: str = mei_file.split("_")[-1].split(".")[0]
+                tokenizer = MEITokenizer(
+                    path.join(manuscript_mei_path, mei_file),
+                    min_ngram=options["min_ngram"],
+                    max_ngram=options["max_ngram"],
+                )
+                ngram_docs = tokenizer.create_ngram_documents()
+                for doc in ngram_docs:
+                    doc["manuscript_id"] = manuscript_id
+                    doc["folio"] = folio_number
+                    doc["image_uri"] = folio_map.get(folio_number, "")
+                solr_conn.add_many(ngram_docs)
+                solr_conn.commit()
+
+    def flush_manuscript_ngrams_from_index(
+        self, solr_conn: SolrConnection, manuscript_id: int
+    ) -> None:
+        """
+        Deletes all n-gram documents for a given manuscript from the Solr index.
+        """
+        solr_conn.delete_query(f"type:omr_ngram AND manuscript_id:{manuscript_id}")
+        solr_conn.commit()
