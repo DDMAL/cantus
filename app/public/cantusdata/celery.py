9a2199866801b416bbf20c9bb016605934ec3663
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-05-27 12:38:39 -0400

    Simplify how celery gets settings

--- app/public/cantusdata/celery.py
+++ app/public/cantusdata/celery.py
@@ -1,9 +1,7 @@
-import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cantusdata.settings")
 app = Celery("cantusdata")
-import cantusdata.settings as settings
+from . import settings
 
 app.config_from_object(settings, namespace="CELERY")
 app.autodiscover_tasks()
