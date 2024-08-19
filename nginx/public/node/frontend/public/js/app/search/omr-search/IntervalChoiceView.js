231ea4acf554e079ec73a333ce4437f0de77f20b
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-08-19 11:28:05 -0400

    feat(interval omr search): add frontend components for interval search
    - Adds interval search to the OMRSearchProvider
    - Creates front-end helpers for entering interval search

---
+++ nginx/public/node/frontend/public/js/app/search/omr-search/IntervalChoiceView.js
@@ -0,0 +1,16 @@
+import Marionette from 'marionette';
+
+import template from './interval-choice-list.template.html';
+
+export default Marionette.CompositeView.extend({
+    template,
+
+    events: {
+        'click [data-search-value]': 'onChoiceClicked'
+    },
+
+    onChoiceClicked: function (event)
+    {
+        this.trigger('use:interval', $(event.target).attr('data-search-value'));
+    }
+});
