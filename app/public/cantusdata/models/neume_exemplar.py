912b260bb1cd9c170b283ff5ae6c8b54cf58d864
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-09-27 14:14:31 -0400

    feat(neume exemplars): add functions for selecting and displaying neume exemplars

--- app/public/cantusdata/models/neume_exemplar.py
+++ app/public/cantusdata/models/neume_exemplar.py
@@ -1,10 +1,15 @@
+from typing import Any
+
 from django.db import models
 from django.utils.html import format_html
 from django.contrib import admin
 
 from cantusdata.models.folio import Folio
+from cantusdata.helpers.iiif_helpers import construct_image_api_url
+from cantusdata.helpers.neume_helpers import NEUME_NAMES
 
 ADMIN_IMAGE_HEIGHT = 100
+EXEMPLAR_IMAGE_SIDE_LENGTH = 80
 
 
 class NeumeExemplar(models.Model):
@@ -17,7 +22,7 @@ class NeumeExemplar(models.Model):
 
     class Meta:
         app_label = "cantusdata"
-        ordering = ["name"]
+        ordering = ["order"]
 
     name = models.CharField(max_length=255, blank=False, null=False)
     folio = models.ForeignKey(Folio, on_delete=models.CASCADE)
@@ -26,6 +31,11 @@ class NeumeExemplar(models.Model):
     y_coord = models.IntegerField()
     width = models.IntegerField()
     height = models.IntegerField()
+    order = models.IntegerField(
+        help_text="""A helper field used to order the exemplars.
+         See helpers/neume_helpers.py for an explanation of how this is used.""",
+        default=1,
+    )
 
     @admin.display(description="Image")
     def admin_image(self) -> str:
@@ -34,13 +44,27 @@ class NeumeExemplar(models.Model):
 
         NOTE: This is intended for use in the admin interface, not the client
         """
-        return format_html(
-            "<img src={}/{},{},{},{}/,{}/0/default.jpg alt={}/>",
-            self.folio.image_uri,
-            self.x_coord,
-            self.y_coord,
-            self.width,
-            self.height,
-            ADMIN_IMAGE_HEIGHT,
-            self.name,
-        )
+        image_uri = self.folio.image_uri
+        # If a neume exemplar has been chosen, the associated folio should have an image,
+        # but in case the uri was removed after selection, we should check for it here
+        if image_uri:
+            image_url = construct_image_api_url(
+                image_uri,
+                region=f"{self.x_coord},{self.y_coord},{self.width},{self.height}",
+                size=f"{ADMIN_IMAGE_HEIGHT},",
+            )
+            return format_html(
+                "<img src={} alt={}/>",
+                image_url,
+                self.name,
+            )
+        return ""
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Calculate the "order" field based on the neume name so that the exemplars
+        are ordered consistently.
+        """
+        # Make the value of order the 1-indexed position of the neume in the list of neumes
+        self.order = NEUME_NAMES.index(self.name) + 1
+        super().save(*args, **kwargs)
--- app/public/cantusdata/serializers/neume_exemplar.py
+++ app/public/cantusdata/serializers/neume_exemplar.py
@@ -1,17 +1,45 @@
-from cantusdata.models.neume_exemplar import NeumeExemplar
+from typing import cast
+
 from rest_framework import serializers
+from django.db.models.query import QuerySet
+from cantusdata.models import NeumeExemplar, Folio
 
 
-class NeumeExemplarSerializer(serializers.ModelSerializer):
+class NeumeExemplarListSerializer(serializers.ListSerializer[NeumeExemplar]):
+    def update(  # type: ignore
+        self,
+        instance: QuerySet[NeumeExemplar],
+        validated_data: list[dict[str, str | int]],
+    ) -> QuerySet[NeumeExemplar]:
+        instance_mapping = {
+            neume_exemplar.name: neume_exemplar for neume_exemplar in instance
+        }
+        for data in validated_data:
+            # We know the value of the "name" key is a string
+            neume_name = cast(str, data["name"])
+            neume_exemplar = instance_mapping.get(neume_name, None)
+            if neume_exemplar is not None:
+                self.child.update(neume_exemplar, data)  # type: ignore[union-attr]
+
+        return instance.all()
+
+
+class NeumeExemplarSerializer(serializers.ModelSerializer[NeumeExemplar]):
     class Meta:
         model = NeumeExemplar
-        fields = ("name", "siglum_slug", "p", "x", "y", "w", "h")
+        fields = ["manuscript", "folio", "name", "p", "x", "y", "w", "h"]
+        list_serializer_class = NeumeExemplarListSerializer
 
-    siglum_slug = serializers.SlugField(source="folio.manuscript.siglum_slug")
-
+    manuscript: str = serializers.StringRelatedField(source="folio.manuscript.name")  # type: ignore
+    folio: str = serializers.StringRelatedField(source="folio.number")  # type: ignore
     # These short forms match the values given in the boxes
     # in Solr OMR results
-    p = serializers.CharField(source="folio.image_uri")
+    p = serializers.SlugRelatedField(
+        slug_field="image_uri",
+        queryset=Folio.objects.all(),
+        source="folio",
+        style={"base_template": "input.html"},
+    )
     x = serializers.IntegerField(source="x_coord")
     y = serializers.IntegerField(source="y_coord")
     w = serializers.IntegerField(source="width")
