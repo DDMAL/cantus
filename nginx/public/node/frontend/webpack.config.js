DDMAL/cantus

commit 9f0372e3ffd6fd0828e145bb942998dd9ba73336

build(frontend): upgrade build process for frontend

nginx/public/node/frontend/.babelrc
@@ -1,3 +1,5 @@
 {
-    "presets": ["es2015"]
+    "presets": [
+        "env"
+    ]
 }

nginx/public/node/frontend/webpack.config.js
@@ -2,25 +2,26 @@
 
 var path = require('path'),
     webpack = require('webpack'),
-    yargs = require('yargs').argv;
+    yargs = require('yargs').argv,
+    TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+};;
 
 var APP_BASE_DIR = path.resolve(__dirname, 'public/js'),
     APP_DIR = path.resolve(APP_BASE_DIR, 'app'),
     LIB_DIR = path.resolve(__dirname, 'public/node_modules'),
     TMP_DIR = path.resolve(__dirname, '.tmp');
 
-function configureBuildMode(config)
-{
-    if (yargs.release)
-    {
-        config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }));
+function configureBuildMode(config) {
+    if (yargs.release) {
+        config.mode = 'production';
     }
-    else
-    {
+    else {
         config.devtool = 'cheap-module-source-map';
     }
 
@@ -43,28 +44,33 @@ module.exports = configureBuildMode({
     },
 
     resolve: {
-        root: [APP_DIR, LIB_DIR, TMP_DIR],
+        modules: [APP_DIR, LIB_DIR, TMP_DIR],
 
         alias: {
             marionette: 'backbone.marionette',
 
             // Alias the Diva path to make it easier to access the plugins, etc.
-            diva: "diva.js/js"
-        }
+            diva: "diva.js/js/diva.js",
+            bootstrap: "bootstrap/dist/js/bootstrap",
+        },
+
     },
 
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 include: [APP_BASE_DIR],
-                loader: 'babel-loader'
+                use: 'babel-loader'
             },
 
             {
                 test: /\.template\.html$/,
                 include: [APP_BASE_DIR],
-                loader: 'underscore-template-loader?engine=underscore'
+                loader: 'underscore-template-loader',
+                options: {
+                    engine: 'underscore'
+                }
             }
         ]
     },
@@ -79,14 +85,7 @@ module.exports = configureBuildMode({
             'window.jQuery': 'jquery',
             $: 'jquery'
         }),
+    ],
 
-        // Resolve main files which differ from the package.json entries
-        new webpack.ResolverPlugin([
-            // Bootstrap only has a main entry for jspm
-            new webpack.ResolverPlugin.FileAppendPlugin(['/dist/js/bootstrap.js']),
-
-            // We've aliased Diva's path so there's no main to look for
-            new webpack.ResolverPlugin.FileAppendPlugin(['/diva.js'])
-        ])
-    ]
+    mode: 'development',
 });

commit 8a4110297c2f295ea5778e5334782f15d1449aa8

fix(frontend dependencies): update babel configuration
Initial frontend builds were failing after 9f0372e3ffd6fd0828e145bb942998dd9ba73336. This updates the babel configuration for correct building.

nginx/public/node/frontend/.babelrc
@@ -1,5 +1,0 @@
-{
-    "presets": [
-        "env"
-    ]
-}

nginx/public/node/frontend/webpack.config.js
@@ -52,7 +52,19 @@ module.exports = configureBuildMode({
             {
                 test: /\.js$/,
                 include: [APP_BASE_DIR],
-                use: 'babel-loader'
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: [
+                            "> 1%",
+                            "last 2 versions",
+                            "Firefox ESR",
+                            "Opera 12.1",
+                            "ie >= 9"
+                        ],
+                        presets: ['@babel/preset-env']
+                    }
+                }
             },
 
             {