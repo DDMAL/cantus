e029064382c712dd84d1c6690b4a58194ef15784
Dylan Hillerbrand <dhillerbrand@gmail.com>
2025-01-23 10:54:50 -0500

    build: stop requiring pin for runserver_plus

--- app/django-config.sh
+++ app/django-config.sh
@@ -4,7 +4,7 @@ python manage.py clear_session_data
 python manage.py collectstatic --noinput
 
 if [[ $DEVELOPMENT == "True" ]]; then
-    python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:3000 manage.py runserver_plus 0:8001
+    python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:3000 manage.py runserver_plus 0:8001 --nopin
 else
     gunicorn -b 0:8001 cantusdata.wsgi --timeout 600 --workers 4
 fi
